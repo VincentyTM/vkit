ab7866e2ddf8c706a1f197c0249ca7a062f091a6
Vincenty <vincenty97@gmail.com>
2021-08-07 14:18:46 +0200

    Add mobile-web-app-capable meta tag

--- vkit-cli/index-html.js
+++ vkit-cli/index-html.js
@@ -3,6 +3,7 @@ module.exports = `<!DOCTYPE html>
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="viewport-fit=cover, minimal-ui, width=device-width, initial-scale=1, user-scalable=yes">
+<meta name="mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="theme-color" content="#ffffff">
 <title>vKit Application</title>
