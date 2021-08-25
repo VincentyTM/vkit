d40e43dcb322ac91d16b7a7cf1558c2553fbc215
Vincenty <vincenty97@gmail.com>
2021-08-25 03:06:34 +0200

    Cross-browser $.html

--- vkit/html.js
+++ vkit/html.js
@@ -30,7 +30,7 @@ function deepInsert(object, parent, nextSibling){
 
 $.html=function(){
 	var result = [], operators = [];
-	var placeholder = "<!---->";
+	var placeholder = "_<!---->";
 	for(var i=0, l=arguments.length; i<l; ++i){
 		var arg = arguments[i];
 		if( arg===null || arg===undefined ) continue;
@@ -73,6 +73,14 @@ $.html=function(){
 	for(i=0, l=operators.length; i<l; ++i){
 		var operator = operators[i];
 		var comment = comments[i];
+		
+		var prevNode = comment.previousSibling, prevText = prevNode.nodeValue;
+		if( prevText.length > 1 ){
+			prevNode.nodeValue = prevText.substring(0, prevText.length - 1);
+		}else{
+			prevNode.parentNode.removeChild(prevNode);
+		}
+		
 		if( typeof operator==="function" ){
 			while( comment && comment.nodeType==8 ){
 				var prev = comment;
