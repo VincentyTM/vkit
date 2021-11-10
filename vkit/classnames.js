926722284cf59c824bf83b01992ec25bd8dfa6c8
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2021-11-10 17:14:01 +0100

    Create classNames module

---
+++ vkit/classnames.js
@@ -0,0 +1,51 @@
+(function($){
+
+var addEffect = $.effect;
+var createState = $.state;
+var createObservable = $.observable;
+
+function classNames(map){
+	function getValue(){
+		var array = [];
+		for(var cname in map){
+			if( get(map[cname]) ){
+				array.push(cname);
+			}
+		}
+		return array.join(" ");
+	}
+	function get(value){
+		if( value && typeof value.get === "function" ){
+			return value.get();
+		}
+		if( typeof value === "function" ){
+			return value();
+		}
+		return value;
+	}
+	var state = createState(getValue());
+	function updateValue(){
+		state.set(getValue());
+	}
+	var unsubscribe = createObservable();
+	state.unsubscribe = unsubscribe;
+	var effect = false;
+	for(var cname in map){
+		var value = map[cname];
+		if( value && value.onChange && typeof value.onChange.subscribe === "function" ){
+			unsubscribe.subscribe(
+				value.onChange.subscribe(updateValue)
+			);
+		}else if( typeof value === "function" ){
+			effect = true;
+		}
+	}
+	if( effect ){
+		addEffect(updateValue);
+	}
+	return state;
+}
+
+$.classNames = classNames;
+
+})($);
