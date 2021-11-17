b8c23019ee5d7a619a24879bf443ef2a3c9ab840
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2021-11-17 17:46:18 +0100

    Move setProps into a separate module

--- vkit/htmltag.js
+++ vkit/htmltag.js
@@ -1,42 +1,17 @@
-(function($, undefined){
+(function($){
 
-function setProps(el, props){
-	for(var prop in props){
-		var val = props[prop];
-		if( prop.indexOf("on") === 0 ){
-			$.on(prop.substring(2), val)(el);
-		}else if( prop === "style" ){
-			for(var cssProp in val){
-				var cssVal = val[cssProp];
-				if( typeof cssVal === "function" ){
-					$.css(cssProp, cssVal)(el);
-				}else if( cssVal && cssVal.css ){
-					cssVal.css(cssProp)(el);
-				}else{
-					el.style[cssProp] = cssVal;
-				}
-			}
-		}else{
-			if( typeof val === "function" ){
-				$.prop(prop, val)(el);
-			}else if( val && val.prop ){
-				val.prop(prop)(el);
-			}else{
-				el[prop] = val;
-			}
-		}
-	}
-}
+var setProps = $.setProps;
+var append = $.append;
 
-$.setProps = setProps;
-
-$.htmlTag = function(tagName){
+function htmlTag(tagName){
 	return function(){
 		var el = document.createElement(tagName);
-		$.append(el, arguments, setProps);
+		append(el, arguments, setProps);
 		return el;
 	};
-};
+}
+
+$.htmlTag = htmlTag;
 
 if( typeof Proxy === "function" ){
 	$.htmlTags = new Proxy({}, {
