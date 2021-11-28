7068d07ff8aa8e456a12cb7680806d3c5b82d579
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2021-11-28 19:15:53 +0100

    Create lazy module

---
+++ vkit/lazy.js
@@ -0,0 +1,32 @@
+(function($){
+
+var render = $.render;
+var createState = $.state;
+var createScript = $.script;
+
+function call(component){
+	return component();
+}
+
+function emptyComponent(){
+	return null;
+}
+
+function lazyComponent(promise, pendingComponent, errorComponent){
+	if(!errorComponent) errorComponent = emptyComponent;
+	var state = createState(pendingComponent || emptyComponent);
+	function onLoad(successComponent){
+		state.set(successComponent);
+		render();
+	}
+	function onError(ex){
+		state.set(function(){ return errorComponent(ex) });
+		render();
+	}
+	(promise && typeof promise.then === "function" ? promise : createScript(promise)).then(onLoad, onError);
+	return state.view(call);
+}
+
+$.lazy = lazyComponent;
+
+})($);
