d7e8ca8c4eeaf530ee19fc3f0746b3e1dd59bd41
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2021-12-03 00:17:33 +0100

    Fix hydrate behavior

--- vkit/hydrate.js
+++ vkit/hydrate.js
@@ -1,38 +1,28 @@
 (function($, undefined){
 
-function deepInsertBefore(anchor, elements){
-	if( elements === null || elements === undefined ){
-		return;
-	}
-	if( typeof elements !== "object" ){
-		anchor.parentNode.insertBefore(document.createTextNode(elements), anchor);
-		return;
-	}
-	if( elements.nodeType ){
-		anchor.parentNode.insertBefore(elements, anchor);
-		return;
-	}
-	for(var i=0, n=elements.length; i<n; ++i){
-		deepInsertBefore(anchor, elements[i]);
-	}
-}
+var append = $.append;
 
-$.fn.hydrate = function(map){
+function hydrate(map){
 	for(var tagName in map){
 		for(var i=this.length; i--;){
 			var container = this[i];
 			if(!container.getElementsByTagName){
 				continue;
 			}
+			var component = map[tagName];
 			var elements = container.getElementsByTagName(tagName);
 			for(var j=elements.length; j--;){
 				var element = elements[j];
-				deepInsertBefore(element, map[tagName](element));
-				element.parentNode.removeChild(element);
+				append(element, component(element));
+			}
+			if( container.tagName === tagName.toUpperCase() ){
+				append(container, component(container));
 			}
 		}
 	}
 	return this;
-};
+}
+
+$.fn.hydrate = hydrate;
 
 })($);
