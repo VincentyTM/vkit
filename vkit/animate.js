296764b0177cb67464f7cd9d88bea5490e1cc341
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2021-12-19 23:40:03 +0100

    Create animate module

---
+++ vkit/animate.js
@@ -0,0 +1,66 @@
+(function($, window){
+
+var createObservable = $.observable;
+var requestAnimationFrame = window.requestAnimationFrame || window.setTimeout;
+var cancelAnimationFrame = window.cancelAnimationFrame || window.clearTimeout;
+var performance = window.performance && window.performance.now ? window.performance : {
+	now: function(){ return new Date().getTime() }
+};
+
+function animate(update, duration, timeSource){
+	if(!timeSource) timeSource = performance;
+	var startTime = timeSource.now();
+	var frame = 0;
+	var lastUpdate = startTime;
+	var onComplete = createObservable();
+	var paused = true;
+	function loop(){
+		lastUpdate = timeSource.now();
+		var dt = lastUpdate - startTime;
+		if( dt < duration ){
+			frame = requestAnimationFrame(loop, 16);
+			update(dt/duration, dt, duration);
+		}else{
+			paused = true;
+			update(1, duration, duration);
+			onComplete();
+		}
+	}
+	function stop(){
+		cancelAnimationFrame(frame);
+		paused = true;
+	}
+	function start(){
+		startTime = timeSource.now();
+		if( paused ){
+			paused = false;
+			requestAnimationFrame(loop, 16);
+		}
+	}
+	function resume(){
+		startTime += timeSource.now() - lastUpdate;
+		if( paused ){
+			paused = false;
+			requestAnimationFrame(loop, 16);
+		}
+	}
+	function toggle(){
+		paused ? resume() : stop();
+	}
+	function isPaused(){
+		return paused;
+	}
+	start();
+	return {
+		then: onComplete.subscribe,
+		stop: stop,
+		start: start,
+		resume: resume,
+		toggle: toggle,
+		isPaused: isPaused
+	};
+}
+
+$.animate = animate;
+
+})($, window);
