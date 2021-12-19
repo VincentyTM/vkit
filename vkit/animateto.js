ce347ad06eea07a1aa6c1e9130c420784a6cdb64
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2021-12-19 23:40:51 +0100

    Create animateTo module

---
+++ vkit/animateto.js
@@ -0,0 +1,39 @@
+(function($){
+
+var render = $.render;
+var unmount = $.unmount;
+var animate = $.animate;
+var createState = $.state;
+
+function animateTo(state, duration, options){
+	var latest = +state.get();
+	var start = latest;
+	var end = latest;
+	var currentState = createState(latest);
+	var currentAnimation = null;
+	var easing = options ? options.easing : null;
+	function animationLoop(t){
+		latest = start + (end - start) * (easing ? easing(t) : t);
+		currentState.set(latest);
+		if( t >= 1 ){
+			currentAnimation = null;
+			start = end = +state.get();
+		}
+		render();
+	}
+	unmount(
+		state.onChange.subscribe(function(value){
+			if( currentAnimation ){
+				currentAnimation.stop();
+			}
+			start = latest;
+			end = +value;
+			currentAnimation = animate(animationLoop, duration * Math.abs(end - start));
+		})
+	);
+	return currentState;
+}
+
+$.animateTo = animateTo;
+
+})($);
