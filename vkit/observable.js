530dcbfc3740e646f569cc2b7650070d129d69f9
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2021-12-29 22:18:16 +0100

    Optimize observable, add count method

--- vkit/observable.js
+++ vkit/observable.js
@@ -1,20 +1,36 @@
 (function($){
 
-$.observable = function(){
-	var subscriptions = {};
-	var id = 0;
-	function render(){
-		for(var id in subscriptions)
-			subscriptions[id].apply(null, arguments);
+function createObservable(){
+	var subscriptions = [], callbacks = [], n = 0;
+	function observable(){
+		for(var i=0; i<n; ++i){
+			callbacks[i].apply(null, arguments);
+		}
 	}
-	render.subscribe = function(fn){
-		var cid = ++id;
-		subscriptions[cid] = fn;
-		return function(){
-			delete subscriptions[cid];
-		};
-	};
-	return render;
-};
+	function subscribe(callback){
+		function unsubscribe(){
+			for(var i=Math.min(curr, n); i--;){
+				if( subscriptions[i] === unsubscribe ){
+					subscriptions.splice(i, 1);
+					callbacks.splice(i, 1);
+					--n;
+					break;
+				}
+			}
+		}
+		subscriptions.push(unsubscribe);
+		callbacks.push(callback);
+		var curr = ++n;
+		return unsubscribe;
+	}
+	function getCount(){
+		return n;
+	}
+	observable.subscribe = subscribe;
+	observable.count = getCount;
+	return observable;
+}
+
+$.observable = createObservable;
 
 })($);
