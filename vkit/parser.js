5961deabca62d8c51f8d864d688d9e22dac3de1d
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2022-01-11 14:13:25 +0100

    Update parser
    * $.parse will throw syntax errors
    * $.parse will not return anything
    * fix missing skipToken function
    * fix expecting stack order

--- vkit/parser.js
+++ vkit/parser.js
@@ -1,54 +1,57 @@
 (function($){
 
-$.parse = function(lexer, expecting, map, applyRule){
-	expecting = expecting.slice(0);
-	try{
-		while( expecting.length ){
-			
-			var expect = expecting.pop();
+var push = Array.prototype.push;
 
-			if( lexer.length===0 || lexer.ended && lexer.ended() ){
-				if( expect.charAt(expect.length-1)==="?" )
-					continue;
-				throw "Unexpected end of file, still expecting '" + expect + "'.";
-			}
+function createError(lexer, message){
+	return new SyntaxError("SyntaxError at line " + lexer.line() + ", char " + lexer.charPos() + ": " + message);
+}
 
-			var node = lexer.shift();
-			var lex = node.type || node;
+function parse(lexer, expecting, map, applyRule, skipToken){
+	expecting = expecting.slice().reverse();
+	while( expecting.length ){
+		var expect = expecting.pop();
 
-			if( expect===lex ){
-				if( applyRule )
-					applyRule(expect, node, null);
+		if( lexer.length === 0 || lexer.ended && lexer.ended() ){
+			if( expect.charAt(expect.length - 1) === "?" ){
 				continue;
 			}
-			
-			if( expect in map ){
-				if( lex in map[expect] ){
-					Array.prototype.push.apply(expecting, map[expect][lex].reverse());
-					if( applyRule )
-						applyRule(expect, node, map[expect][lex]);
-					continue;
-				}
-				if( expect.charAt(expect.length-1)==="?" ){
-					lexer.unshift(node);
-					if( applyRule )
-						applyRule(expect, null, null);
-					continue;
-				}
-				throw "Invalid token '" + node + "', expecting '" + expect + "'.";
-			}else{
-				throw "Unexpected token '" + node + "', expecting '" + expect + "'.";
+			throw createError(lexer, "Unexpected end of input, still expecting '" + expect + "'.");
+		}
+
+		var node = lexer.shift();
+		var lex = node.type || node;
+
+		if( skipToken && skipToken(node) ){
+			expecting.push(expect);
+		}else if( expect === lex ){
+			if( applyRule ){
+				applyRule(expect, node, null);
 			}
+		}else if( expect in map ){
+			if( lex in map[expect] ){
+				push.apply(expecting, map[expect][lex].reverse());
+				if( applyRule ){
+					applyRule(expect, node, map[expect][lex]);
+				}
+			}else if( expect.charAt(expect.length - 1) === "?" ){
+				lexer.unshift(node);
+				if( applyRule ){
+					applyRule(expect, null, null);
+				}
+			}else{
+				throw createError(lexer, "Invalid token '" + node + "', expecting '" + expect + "'.");
+			}
+		}else{
+			throw createError(lexer, "Unexpected token '" + node + "', expecting '" + expect + "'.");
 		}
-		if( lexer.length || lexer.ended && !lexer.ended() ){
-			var next = lexer.shift();
-			lexer.unshift(next);
-			throw "Unnecessary tokens starting with '" + next + "'.";
-		}
-		return "Successfully parsed.";
-	}catch(ex){
-		return "SyntaxError at line " + lexer.line() + ", char " + lexer.charPos() + ": " + ex;
 	}
-};
+	if( lexer.length || lexer.ended && !lexer.ended() ){
+		var next = lexer.shift();
+		lexer.unshift(next);
+		throw createError(lexer, "Unnecessary tokens starting with '" + next + "'.");
+	}
+}
+
+$.parse = parse;
 
 })($);
