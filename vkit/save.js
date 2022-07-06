de486aa66d41b3b919d550c286467090f088b219
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2022-07-06 10:49:42 +0200

    Add webkitURL fallback

--- vkit/save.js
+++ vkit/save.js
@@ -1,6 +1,11 @@
-(function($, document){
+(function($, document, global){
+
+var URL = global.URL || global.webkitURL;
 
 function save(blob, name){
+	if(!URL){
+		throw new ReferenceError("URL is not supported");
+	}
 	var url = URL.createObjectURL(blob);
 	var a = document.createElement("a");
 	a.href = url;
@@ -13,4 +18,4 @@ function save(blob, name){
 
 $.save = save;
 
-})($, document);
+})($, document, this);
