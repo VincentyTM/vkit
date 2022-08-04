8115445c63a23555b9288d515802d687db3f0199
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2022-08-04 11:20:21 +0200

    Create fullScreenState module

---
+++ vkit/fullscreenstate.js
@@ -0,0 +1,45 @@
+(function($, document){
+
+var createState = $.state;
+var onEvent = $.onEvent;
+var unmount = $.unmount;
+
+function createFullScreenState(doc){
+	if(!doc){
+		doc = document;
+	}
+	var state = createState();
+	function updateState(){
+		state.set(
+			doc.fullscreenElement ||
+			doc.mozFullScreenElement ||
+			doc.webkitFullscreenElement ||
+			doc.webkitCurrentFullScreenElement ||
+			null
+		);
+	}
+	updateState();
+	state.effect(function(el){
+		if( el ){
+			el.requestFullscreen ? el.requestFullscreen() :
+			el.mozRequestFullScreen ? el.mozRequestFullScreen() :
+			el.webkitRequestFullscreen ? el.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT) :
+			el.webkitEnterFullscreen ? el.webkitEnterFullscreen() :
+			state.set(null);
+		}else{
+			doc.cancelFullScreen ? doc.cancelFullScreen() :
+			doc.mozCancelFullScreen ? doc.mozCancelFullScreen() :
+			doc.webkitCancelFullScreen ? doc.webkitCancelFullScreen() :
+			doc.exitFullscreen ? doc.exitFullscreen() :
+			doc.webkitExitFullscreen ? doc.webkitExitFullscreen() :
+			null;
+		}
+	});
+	unmount(onEvent(doc, "fullscreenchange", updateState));
+	unmount(onEvent(doc, "webkitfullscreenchange", updateState));
+	return state;
+}
+
+$.fullScreenState = createFullScreenState;
+
+})($, document);
