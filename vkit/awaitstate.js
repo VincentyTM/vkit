0d0c17f3f0fbd45afb5876a553531f026cb17c9c
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2022-09-23 10:49:07 +0200

    Allow null value in awaitState

--- vkit/awaitstate.js
+++ vkit/awaitstate.js
@@ -5,21 +5,25 @@ var render = $.render;
 
 function awaitState(promiseOrState){
 	var state = createState();
-	var lastPromise;
+	var lastPromise = null;
 	function setPromise(promise){
-		lastPromise = promise;
 		state.set({pending: true});
-		promise.then(function(value){
-			if( promise === lastPromise ){
-				state.set({fulfilled: true, value: value});
-				render();
-			}
-		}, function(error){
-			if( promise === lastPromise ){
-				state.set({rejected: true, error: error});
-				render();
-			}
-		});
+		if( promise && typeof promise.then === "function" ){
+			lastPromise = promise;
+			promise.then(function(value){
+				if( promise === lastPromise ){
+					state.set({fulfilled: true, value: value});
+					render();
+				}
+			}, function(error){
+				if( promise === lastPromise ){
+					state.set({rejected: true, error: error});
+					render();
+				}
+			});
+		}else{
+			lastPromise = null;
+		}
 	}
 	if( promiseOrState.effect ){
 		promiseOrState.effect(setPromise);
