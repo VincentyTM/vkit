30dffc453c52872590b120e316c91693dd6d681a
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2022-09-29 17:51:39 +0200

    Fix double rendering of queryParamsState

--- vkit/queryparamsstate.js
+++ vkit/queryparamsstate.js
@@ -11,27 +11,25 @@ function createQueryParamsState(win){
 	var historyURL = createHistoryState(win).url;
 	var queryParamsState = url(historyURL).queryParams;
 	return function(name){
-		var state = createState();
-		var set = state.set;
-		queryParamsState.effect(function(queryParams){
-			set( queryParams.get(name) );
+		var state = queryParamsState.map(function(queryParams){
+			return queryParams.get(name);
 		});
-		function setValue(value){
-			set(value);
+		state.set = function(value){
+			historyURL.set(getQuery(value) + location.hash);
+		};
+		state.push = function(value){
+			historyURL.push(getQuery(value) + location.hash);
+		};
+		function getQuery(value){
 			var queryParams = createQueryParams(location.search.substring(1));
 			if( value === null || value === undefined ){
 				queryParams.del(name);
 			}else{
 				queryParams.set(name, value);
 			}
-			return queryParams;
+			var params = queryParams.toString();
+			return params ? "?" + params : "";
 		}
-		state.set = function(value){
-			historyURL.set("?" + setValue(value) + location.hash);
-		};
-		state.push = function(value){
-			historyURL.push("?" + setValue(value) + location.hash);
-		};
 		return state;
 	};
 };
