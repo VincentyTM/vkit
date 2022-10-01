b2819c5416af2955b583a120918180622343e78d
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2022-10-01 11:26:34 +0200

    Add action selection in objectState

--- vkit/objectstate.js
+++ vkit/objectstate.js
@@ -69,6 +69,9 @@ function createObjectState(parent, methods){
 				}
 			});
 		}
+		if( key in methods ){
+			return actions[key];
+		}
 		if(!reducer){
 			reducer = initialValue;
 			initialValue = undefined;
