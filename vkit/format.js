f854f21cbec4e5d378ad2b23daf2b3fcad2fbe0d
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2022-11-18 17:03:02 +0100

    Add format offset parameter

--- vkit/format.js
+++ vkit/format.js
@@ -1,28 +1,31 @@
 (function($){
 
-function splitText(text, pattern, transformIn, transformOut, nodes){
+function splitText(text, pattern, transformIn, transformOut, nodes, offset){
 	var match, last = 0;
 	while( null !== (match = pattern.exec(text)) ){
 		var a = pattern.lastIndex - match[0].length;
 		var b = pattern.lastIndex;
 		var matchedText = text.substring(a, b);
 		nodes.push(
-			transformOut(text.substring(last, a)),
-			transformIn(matchedText)
+			transformOut(text.substring(last, a), offset + last),
+			transformIn(matchedText, offset + a)
 		);
 		last = b;
 	}
 	if( last < text.length ){
 		nodes.push(
-			transformOut(text.substring(last))
+			transformOut(text.substring(last), offset + last)
 		);
 	}
 	return nodes;
 }
 
-function formatText(text, maps, index){
+function formatText(text, maps, index, offset){
 	if(!index){
 		index = 0;
+	}
+	if(!offset){
+		offset = 0;
 	}
 	if( index >= maps.length ){
 		return text;
@@ -32,11 +35,12 @@ function formatText(text, maps, index){
 		text,
 		map.pattern,
 		map.component,
-		function(subtext){
-			return formatText(subtext, maps, index + 1);
+		function(subtext, innerOffset){
+			return formatText(subtext, maps, index + 1, innerOffset);
 		},
-		[]
-	) : map.component(text);
+		[],
+		offset
+	) : map.component(text, offset);
 }
 
 $.format = formatText;
