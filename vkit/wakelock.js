74fb9934327c11ed572e989a57a62d73b2068a8c
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2022-12-10 18:57:13 +0100

    Create wakeLock module

---
+++ vkit/wakelock.js
@@ -0,0 +1,57 @@
+(function($, navigator){
+
+var render = $.render;
+var createState = $.state;
+var unmount = $.unmount;
+
+function wakeLock(controller){
+	var currentSentinel = createState(null);
+	var isPending = false;
+	
+	function lock(){
+		if( navigator.wakeLock && !isPending && !currentSentinel.get() ){
+			isPending = true;
+			navigator.wakeLock.request("screen").then(function(sentinel){
+				isPending = false;
+				if(!sentinel.released){
+					sentinel.onrelease = function(){
+						currentSentinel.set(null);
+						render();
+					};
+					currentSentinel.set(sentinel);
+					render();
+				}
+			}, function(err){
+				isPending = false;
+				currentSentinel.set(null);
+				render();
+			});
+		}
+	}
+	
+	function unlock(){
+		var sentinel = currentSentinel.get();
+		if( sentinel && !isPending ){
+			isPending = true;
+			sentinel.release().then(function(){
+				isPending = false;
+				currentSentinel.set(null);
+				render();
+			}, function(){
+				isPending = false;
+			});
+		}
+	}
+	
+	controller.map(Boolean).effect(function(doLock){
+		doLock ? lock() : unlock();
+	});
+	
+	unmount(unlock);
+	
+	return currentSentinel.map(Boolean);
+}
+
+$.wakeLock = wakeLock;
+
+})($, navigator);
