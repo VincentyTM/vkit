15ee07e3c1dd92ece6c41616f70cccebe52b80c8
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-01-08 12:07:27 +0100

    Optimize observable

--- vkit/observable.js
+++ vkit/observable.js
@@ -1,33 +1,42 @@
 (function($){
 
 function createObservable(){
-	var subscriptions = [], callbacks = [], n = 0;
+	var callbacks = [], n = 0;
+	
 	function observable(){
-		for(var i=0; i<n; ++i){
-			callbacks[i].apply(null, arguments);
+		for(var i=0; i<n; i+=2){
+			callbacks[i].apply(this, arguments);
 		}
 	}
+	
 	function subscribe(callback){
 		function unsubscribe(){
 			for(var i=Math.min(curr, n); i--;){
-				if( subscriptions[i] === unsubscribe ){
-					subscriptions.splice(i, 1);
-					callbacks.splice(i, 1);
-					--n;
+				if( callbacks[i] === unsubscribe ){
+					callbacks.splice(i - 1, 2);
+					n -= 2;
 					break;
 				}
 			}
 		}
-		subscriptions.push(unsubscribe);
-		callbacks.push(callback);
-		var curr = ++n;
+		callbacks.push(callback, unsubscribe);
+		var curr = n += 2;
 		return unsubscribe;
 	}
+	
 	function getCount(){
-		return n;
+		return n / 2;
 	}
+	
+	function clear(){
+		callbacks.splice(0, n);
+		n = 0;
+	}
+	
 	observable.subscribe = subscribe;
 	observable.count = getCount;
+	observable.clear = clear;
+	
 	return observable;
 }
 
