e44093e522faa64b9de9354a4f91da249cf7b8d2
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-02-17 23:17:27 +0100

    Change customElement behavior
    * Each custom element has its own component tree
    * The connect and disconnect callbacks are handled by component creation and unmounting
    * The observed attributes are available as properties of the observedAttributes object
    * The adopt option was renamed to adoptedCallback

--- vkit/customelement.js
+++ vkit/customelement.js
@@ -1,11 +1,12 @@
 (function($, window, undefined){
 
+var bind = $.bind;
 var group = $.group;
 var append = $.append;
 var render = $.render;
-var bind = $.bind;
-var createState = $.state;
-var syncState = $.sync;
+var createComponent = $.component;
+var getCurrentComponent = $.currentComponent;
+var setCurrentComponent = $.setCurrentComponent;
 
 function setPrototypeOf(obj, proto){
 	if( Object.setPrototypeOf ){
@@ -15,80 +16,60 @@ function setPrototypeOf(obj, proto){
 	}
 }
 
-function createCustomElement(name, component, options){
+function createCustomElement(name, getView, options){
 	function CustomElement(){
 		var el = Reflect.construct(win.HTMLElement, [], CustomElement);
-		var attrs = {};
-		el.data = {attributes: attrs};
-
-		function attr(name, defaultValue){
-			if( name === undefined ){
-				return new Proxy({}, {
-					get: getAttr
-				});
-			}
-
-			function onAttrChange(value){
-				if( value === null ){
-					el.removeAttribute(name);
-				}else{
-					el.setAttribute(name, value);
-				}
-			}
-
-			if(!(name in attrs)){
-				var state = createState(el.getAttribute(name));
-				state.onChange.subscribe(onAttrChange);
-				attrs[name] = state;
-			}
-
-			var attrState = attrs[name];
-			if( defaultValue !== undefined && defaultValue !== null ){
-				attrState = syncState(attrState, function(value){
-					return value !== undefined && value !== null ? value : defaultValue;
-				});
-			}
-
-			return attrState;
+		var component = createComponent(null);
+		el.component = component;
+		el.observedAttributes = {};
+		if( CustomElement.observedAttributes ){
+			CustomElement.observedAttributes.forEach(function(attrName){
+				el.observedAttributes[attrName] = el.getAttribute(attrName);
+			});
 		}
-
-		function getAttr(target, prop, receiver){
-			return attr(prop);
-		}
-
-		var view = component(el, attr);
-		append(el, view, el, bind);
 		return el;
 	}
-
-	function onAttr(name, oldValue, newValue){
-		var attr = this.data.attributes[name];
-		if( attr ){
-			attr.set(newValue);
-			render();
-		}
-	}
-
+	
 	var proto = CustomElement.prototype;
 	var win = window;
-
+	
+	proto.connectedCallback = function(){
+		var component = this.component;
+		var prev = getCurrentComponent();
+		setCurrentComponent(component);
+		try{
+			var view = getView.call(this);
+			append(this, [component.start, view, component.end], this, bind);
+		}finally{
+			setCurrentComponent(prev);
+		}
+	};
+	
+	proto.disconnectedCallback = function(){
+		var component = this.component;
+		component.unmount();
+		component.children.splice(0, component.children.length);
+		component.removeView();
+	};
+	
 	if( options ){
 		if( options.window ) win = options.window;
-		if( options.adopt ) proto.adoptedCallback = options.adopt;
-		if( options.connect ) proto.connectedCallback = options.connect;
-		if( options.disconnect ) proto.disconnectedCallback = options.disconnect;
-		if( options.observe ){
-			CustomElement.observedAttributes = options.observe;
-			proto.attributeChangedCallback = onAttr;
+		if( options.adoptedCallback ) proto.adoptedCallback = options.adoptedCallback;
+		if( options.observedAttributes ){
+			CustomElement.observedAttributes = options.observedAttributes;
+			proto.attributeChangedCallback = function(name, oldValue, newValue){
+				this.observedAttributes[name] = newValue;
+				render();
+			};
 		}
 	}
-
-	setPrototypeOf(CustomElement.prototype, win.HTMLElement.prototype);
+	
+	setPrototypeOf(proto, win.HTMLElement.prototype);
 	setPrototypeOf(CustomElement, win.HTMLElement);
-
+	
 	win.customElements.define(name, CustomElement);
-
-	return component;
+	
+	return getView;
 }
 
 $.customElement = createCustomElement;
