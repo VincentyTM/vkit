8546c744a41fedcbc347e252444eacb664a4d3c5
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-02-22 15:54:45 +0100

    Create HTTPS server if secure

--- vkit-cli/server.js
+++ vkit-cli/server.js
@@ -1,4 +1,30 @@
+const fs = require("fs");
 const http = require("http");
+const https = require("https");
+
+const loadCertificate = (privateKey, certificate) => (
+	new Promise((resolve, reject) => {
+		let key, cert;
+		const success = () => key && cert && resolve({
+			key,
+			cert
+		});
+		fs.readFile(privateKey, (err, k) => {
+			if( err ){
+				console.error("Private key not found");
+			}else{
+				success(key = k);
+			}
+		});
+		fs.readFile(certificate, (err, c) => {
+			if( err ){
+				console.error("Certificate not found");
+			}else{
+				success(cert = c);
+			}
+		});
+	})
+);
 
 class Server {
 	constructor(requestListener){
@@ -6,11 +32,18 @@ class Server {
 		this.server = null;
 		this.requestListener = requestListener;
 	}
-		return new Promise((resolve, reject) => {
-	async start({port}){
+	async start(options){
+		return new Promise(async (resolve, reject) => {
+			const {port, secure, privateKey, certificate} = options;
 			this.stop();
-			this.server = http.createServer(this.requestListener).listen({port}, resolve).on("error", err => {
+			this.server = secure
+				? https.createServer(
+					await loadCertificate(privateKey, certificate),
+					this.requestListener
+				)
+				: http.createServer(this.requestListener);
 			this.port = port;
+			this.server.listen({port}, resolve).on("error", err => {
 				if( err.code === "EADDRINUSE" ){
 					console.log("\nPort " + port + " is already in use. You can change it in config.json.");
 					this.start({
