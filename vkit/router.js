7fbb07a263b25d31cf33c70de6556b3c935664e2
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-02-26 00:12:08 +0100

    Simplify router module
    * Remove component method
    * Remove redirectTo method
    * Add currentRoute property
    * Add getRoute method
    * Add render method

--- vkit/router.js
+++ vkit/router.js
@@ -1,82 +1,12 @@
 (function($, undefined){
 
-function createRouter(pathState){
-	function createComponent(routes){
-		routes = routes.slice();
-		var n = routes.length;
-		for(var i=0; i<n; ++i){
-			routes[i] = new Route(routes[i]);
-		}
-		return pathState.view(function(path){
-			var route = getCurrentRoute(routes, path);
-			if(!route){
-				throw new Error("Default route is not set");
-			}
-			var params = typeof route.params === "function" ? route.params(route, path) : route.params;
-			return route ? route.component.apply(null, params) : null;
-		});
-	}
-	
-	function getCurrentRoute(routes, path){
-		var n = routes.length;
-		for(var i=0; i<n; ++i){
-			var route = routes[i];
-			if( route.matches(path) ){
-				return route;
-			}
-		}
-		return null;
-	}
-	
-	function isActive(path, exact){
-		var route = typeof path === "object" ? path : new Route({path: path, exact: exact});
-		return pathState.map(function(currentPath){
-			return route.matches(currentPath);
-		});
-	}
-	
-	function redirectTo(path){
-		return function(){
-			pathState.set(path);
-		};
-	}
-	
-	return {
-		path: pathState,
-		isActive: isActive,
-		redirectTo: redirectTo,
-		component: createComponent
-	};
-}
-
-function Route(route){
-	this.path = route.path;
-	this.exact = route.exact || route.exact === undefined;
-	this.params = route.params || [];
-	this.component = route.component;
-	if( route.matches ) this.matches = route.matches;
-}
-
-Route.prototype.matches = function(path){
-	var routePath = this.path;
-	if( routePath === path || routePath === undefined ){
-		return true;
-	}
-	if( routePath.test ){
-		return routePath.test(path);
-	}
-	var a = routePath.split("/");
-	var b = path.split("/");
-	return this.exact ? equals(a, b) : isPrefixOf(a, b);
-};
-
 function equals(a, b){
 	var n = a.length;
-	if( n!==b.length ){
+	if( n !== b.length ){
 		return false;
 	}
 	for(var i=0; i<n; ++i){
-		if( a[i]!==b[i] && a[i].charAt(0)!==":" ){
+		if( a[i] !== b[i] && a[i].charAt(0) !== ":" ){
 			return false;
 		}
 	}
@@ -85,16 +15,72 @@ function equals(a, b){
 
 function isPrefixOf(a, b){
 	var n = a.length;
-	if( n > b.length )
+	if( n > b.length ){
 		return false;
+	}
 	for(var i=0; i<n; ++i){
-		if( a[i]!==b[i] && a[i].charAt(0)!==":" ){
+		if( a[i] !== b[i] && a[i].charAt(0) !== ":" ){
 			return false;
 		}
 	}
 	return true;
 }
 
-$.router = createRouter;
+function matches(routePath, currentPath, exact){
+	if( routePath === currentPath || routePath === undefined ){
+		return true;
+	}
+	if( typeof routePath.test === "function" ){
+		return routePath.test(currentPath);
+	}
+	if( typeof routePath === "function" ){
+		return routePath(currentPath);
+	}
+	var a = routePath.split("/");
+	var b = currentPath.split("/");
+	return exact !== false ? equals(a, b) : isPrefixOf(a, b);
+}
+
+function Router(pathState, routes){
+	if(!(this instanceof Router)){
+		return new Router(pathState, routes);
+	}
+	
+	function getRoute(path){
+		var n = routes.length;
+		for(var i=0; i<n; ++i){
+			var route = routes[i];
+			if( route && matches(route.path, path, route.exact) ){
+				return route;
+			}
+		}
+		return null;
+	}
+	
+	function isActive(path){
+		var route = getRoute(path);
+		return currentRoute.map(function(r){
+			return r === route;
+		});
+	}
+	
+	function render(){
+		return currentRoute.view(function(route){
+			var component = route.component;
+			var params = route.params;
+			return component ? (params ? component.apply(null, params) : component()) : null;
+		});
+	}
+	
+	var currentRoute = pathState.map(getRoute);
+	
+	this.path = pathState;
+	this.isActive = isActive;
+	this.getRoute = getRoute;
+	this.currentRoute = currentRoute;
+	this.render = render;
+}
+
+$.router = Router;
 
 })($);
