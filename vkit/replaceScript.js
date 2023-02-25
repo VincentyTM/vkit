2047eacc32edb0900bd7079467cd39e44f241feb
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-02-25 19:33:56 +0100

    Use root component in replaceScript

--- vkit/replaceScript.js
+++ vkit/replaceScript.js
@@ -2,11 +2,12 @@
 
 var insert = $.insert;
 var remove = $.remove;
+var renderTree = $.fn.render;
 var getCurrentScript = $.currentScript;
 
 function replaceScript(getView){
 	var script = getCurrentScript();
-	var view = typeof getView === "function" ? getView(script) : getView;
+	var view = typeof getView === "function" ? renderTree.call([], getView) : getView;
 	insert(view, script);
 	remove(script);
 	return view;
