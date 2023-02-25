1bbecbe418e27dd5034f74c73a9a4e900c512658
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-02-25 19:37:29 +0100

    Add state controlled unsavedGuard

--- vkit/unsavedGuard.js
+++ vkit/unsavedGuard.js
@@ -10,10 +10,30 @@ function prevent(e){
 	return "";
 }
 
-function unsavedGuard(win){
-	unmount(
-		onEvent(win || window, "beforeunload", prevent)
-	);
+function unsavedGuard(state, win){
+	if(!win) win = window;
+	var unsubscribe = null;
+	
+	function add(){
+		if(!unsubscribe){
+			unsubscribe = onEvent(win, "beforeunload", prevent);
+		}
+	}
+	
+	function remove(){
+		if( unsubscribe ){
+			unsubscribe();
+			unsubscribe = null;
+		}
+	}
+	
+	function update(value){
+		value ? add() : remove();
+	}
+	
+	state && state.map ? state.map(Boolean).effect(update) : add();
+	unmount(remove);
+	return state;
 }
 
 $.unsavedGuard = unsavedGuard;
