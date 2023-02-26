b89c970117d8d751412a87e65ff7eea63ec97b8f
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-02-26 14:04:14 +0100

    Update hashState with history.pushState

--- vkit/hashState.js
+++ vkit/hashState.js
@@ -3,11 +3,18 @@
 var unmount = $.unmount;
 var onEvent = $.onEvent;
 var createState = $.state;
+var createHistoryState = $.historyState;
+
+function getHash(url){
+	var pos = url.indexOf("#");
+	return pos === -1 ? "" : url.substring(pos + 1);
+}
 
 function createHashState(win){
 	if(!win) win = window;
 	var location = win.location;
 	var state = createState(decodeURIComponent(location.hash.substring(1)));
+	createHistoryState().url.map(getHash).pipe(state);
 	state.onChange.subscribe(function(value){
 		location.replace("#" + encodeURIComponent(value));
 	});
