c70dc5141d882ba7dc49e5a8f61662ca210d0dfd
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-02-28 13:00:14 +0100

    Create httpState module

---
+++ vkit/httpState.js
@@ -0,0 +1,41 @@
+(function($){
+
+var createState = $.state;
+var http = $.http;
+var unmount = $.unmount;
+
+function createHttpState(urlState, options){
+	if(!options) options = {};
+	var state = createState(options.defaultValue);
+	var request = null;
+	
+	function updateValue(res){
+		if( res.status !== 200 ){
+			if( options.onerror ){
+				options.onerror(new Error("HTTP status code is " + res.status));
+			}
+			return;
+		}
+		state.set(res.body);
+	}
+	
+	urlState.effect(function(url){
+		if( request ){
+			request.abort();
+		}
+		request = http(url, options);
+		request.then(updateValue, options.onerror);
+	});
+	
+	unmount(function(){
+		if( request ){
+			request.abort();
+		}
+	});
+	
+	return state.map();
+}
+
+$.httpState = createHttpState;
+
+})($);
