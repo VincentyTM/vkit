10232ff37368bea63450eaaff56445a0a996715e
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-02-28 10:20:01 +0100

    Fix error when rendering $() lists

--- vkit/group.js
+++ vkit/group.js
@@ -1,5 +1,7 @@
 (function($, document, undefined){
 
+var toArray = $.fn.toArray;
+
 function deepPush(array, item, context, bind){
 	if( item === null || item === undefined || typeof item === "boolean" ){
 	}else if( typeof item !== "object" ){
@@ -10,6 +12,12 @@ function deepPush(array, item, context, bind){
 		}
 	}else if( item.nodeType ){
 		array.push(item);
+	}else if( typeof item.length === "number" ){
+		var n = item.length;
+		var a = toArray.call(item);
+		for(var i=0; i<n; ++i){
+			deepPush(array, a[i], context, bind);
+		}
 	}else if( typeof item.render === "function" ){
 		deepPush(array, item.render(), context, bind);
 	}else if( typeof item.text === "function" ){
@@ -20,16 +28,8 @@ function deepPush(array, item, context, bind){
 			x = item.next();
 			deepPush(array, x.value, context, bind);
 		}while(!x.done);
-	}else{
-		var n = item.length;
-		if( typeof n === "number" ){
-			var a = $.fn.toArray.call(item);
-			for(var i=0; i<n; ++i){
-				deepPush(array, a[i], context, bind);
-			}
-		}else if( bind ){
-			bind(context, item, true);
-		}
+	}else if( bind ){
+		bind(context, item, true);
 	}
 	return array;
 }
@@ -38,7 +38,7 @@ function group(){
 	return deepPush($(), arguments, null, null);
 }
 
-$.group = group;
 $.deepPush = deepPush;
+$.group = group;
 
 })($, document);
