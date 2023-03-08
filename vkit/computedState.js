71a3f5767a1e956d272309c03927f8fbe265ce41
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-03-08 16:24:49 +0100

    Add context guard to computedState

--- vkit/computedState.js
+++ vkit/computedState.js
@@ -1,9 +1,14 @@
 (function($){
 
 var createState = $.state;
+var getComponent = $.getComponent;
+var setComponent = $.setComponent;
 
 function createComputedState(getter, updater){
+	var prev = getComponent();
+	setComponent(null);
 	var state = createState(getter());
+	getComponent(prev);
 	updater(function(){
 		state.set(getter());
 	});
