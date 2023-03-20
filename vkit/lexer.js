97d97eaa2a1d9795e89d7ce7905017edfdb7a725
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-03-20 18:47:04 +0100

    Update code style in lexer module

--- vkit/lexer.js
+++ vkit/lexer.js
@@ -3,11 +3,11 @@
 var iterator = typeof Symbol === "function" ? Symbol.iterator : null;
 
 function LexNode(type, word, pos, len){
-	this.type=type;
-	this.word=word;
-	this.pos=pos;
-	this.len=len;
-	this.ref=null;
+	this.type = type;
+	this.word = word;
+	this.pos = pos;
+	this.len = len;
+	this.ref = null;
 }
 
 LexNode.prototype.toString = function(){
@@ -15,78 +15,93 @@ LexNode.prototype.toString = function(){
 };
 
 function Lexer(rules, text){
-	var pos=0;
-	var buffer=[];
+	var pos = 0;
+	var buffer = [];
+	
 	if( iterator ){
 		this[iterator] = function(){
 			return this;
 		};
 	}
-	this.all=function(){
+	
+	this.all = function(){
 		var array = [];
 		while(!this.ended()){
 			array.push(this.shift());
 		}
 		return array;
 	};
-	this.next=function(){
+	
+	this.next = function(){
 		return this.ended() ? {done: true} : {done: false, value: this.shift()};
 	};
-	this.shift=function(){
-		if( buffer.length )
+	
+	this.shift = function(){
+		if( buffer.length ){
 			return buffer.pop();
-		if( pos<text.length ){
-			var ctype, word, len=0, sub=text.substring(pos);
+		}
+		if( pos < text.length ){
+			var ctype, word, len = 0, sub = text.substring(pos);
 			for(var type in rules){
-				var regex=rules[type];
-				var match=regex.exec(sub);
-				if( match && match.index==0 ){
-					if( len<match[0].length ){
-						word=match[0];
-						len=word.length;
-						ctype=type;
+				var regex = rules[type];
+				var match = regex.exec(sub);
+				if( match && match.index === 0 ){
+					if( len < match[0].length ){
+						word = match[0];
+						len = word.length;
+						ctype = type;
 					}
 				}
 			}
-			if(!len)
+			if( len === 0 ){
 				throw new SyntaxError("Illegal character in lexer input.");
-			var node=new LexNode(ctype, word, pos, len);
-			pos+=len;
+			}
+			var node = new LexNode(ctype, word, pos, len);
+			pos += len;
 			return node;
 		}
 		throw new SyntaxError("Reached the end of input. Should not happen.");
 	};
-	this.unshift=function(token){
+	
+	this.unshift = function(token){
 		buffer.push(token);
 	};
-	this.pos=function(){
+	
+	this.pos = function(){
 		return pos;
 	};
-	this.jump=function(p){
-		pos=p;
+	
+	this.jump = function(p){
+		pos = p instanceof LexNode ? p.pos : p;
 	};
-	this.ended=function(){
-		return pos>=text.length && !buffer.length;
+	
+	this.ended = function(){
+		return pos >= text.length && !buffer.length;
 	};
-	this.line=function(token){
-		var p=token ? token.pos : pos;
+	
+	this.line = function(token){
+		var p = token ? token.pos : pos;
 		return text.substring(0, p).split("\n").length;
 	};
-	this.charPos=function(token){
-		var p=token ? token.pos : pos;
+	
+	this.charPos = function(token){
+		var p = token ? token.pos : pos;
 		return p - text.lastIndexOf("\n", p - 1);
 	};
-	this.info=function(token){
+	
+	this.info = function(token){
 		return "'" + (token.word || token.type) + "' at line " + this.line(token) + ", char " + this.charPos(token) + ".";
 	};
 }
 
-$.lexer = function(rules, text){
+function createLexer(rules, text){
 	return arguments.length >= 2 ? new Lexer(rules, text) : {
 		scan: function(text){
 			return new Lexer(rules, text);
 		}
 	};
-};
+}
+
+$.lexer = createLexer;
 
 })($);
