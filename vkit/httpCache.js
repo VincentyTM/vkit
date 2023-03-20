15c68db7081d57c1d227f84466c50fc4925bcbc6
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-03-20 18:56:09 +0100

    Create httpCache module

---
+++ vkit/httpCache.js
@@ -0,0 +1,62 @@
+(function($){
+
+var createState = $.state;
+
+function noop(value){
+	return value;
+}
+
+function httpCache(params){
+	var httpState = params.http;
+	var storageState = params.storage;
+	var defaultValue = params.defaultValue;
+	var parse = typeof params.parse === "function" ? params.parse : noop;
+	var onError = typeof params.onError === "function" ? params.onError : noop;
+	var state = createState(defaultValue);
+	
+	function setValue(value){
+		if( value === null || value === undefined ){
+			state.set(defaultValue);
+		}else{
+			try{
+				state.set(parse(value));
+			}catch(ex){
+				onError(ex);
+			}
+		}
+	}
+	
+	if( storageState ){
+		if( typeof storageState.effect === "function" ){
+			storageState.effect(setValue);
+		}else if( typeof storageState.get === "function" ){
+			setValue(storageState.get());
+		}
+	}
+	
+	if( httpState && typeof httpState.effect === "function" ){
+		httpState.effect(function(res){
+			if(!res || res.progress){
+				return;
+			}
+			if( res.status === 200 ){
+				try{
+					state.set(parse(res.body));
+					if( storageState && typeof storageState.set === "function" ){
+						storageState.set(res.body);
+					}
+				}catch(ex){
+					onError(ex);
+				}
+			}else{
+				onError(new Error("HTTP status is " + res.status));
+			}
+		});
+	}
+	
+	return state.map();
+}
+
+$.httpCache = httpCache;
+
+})($);
