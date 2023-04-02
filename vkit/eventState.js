975e577c160f5bc65bc4f9252a549e404b4accf5
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-04-02 13:26:34 +0200

    Add optional transform parameter to eventState

--- vkit/eventState.js
+++ vkit/eventState.js
@@ -4,10 +4,13 @@ var createState = $.state;
 var onEvent = $.onEvent;
 var unmount = $.unmount;
 
-function createEventState(target, eventType, defaultValue){
+function createEventState(target, eventType, defaultValue, transform){
 	var state = createState(defaultValue);
 	
 	function setter(e){
+		if( typeof transform === "function" ){
+			e = transform(e);
+		}
 		state.set(e);
 	}
 	
