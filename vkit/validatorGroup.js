9e1e81277d7d1af236951d5e14dfb128a9158d91
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-05-03 13:56:25 +0200

    Create validatorGroup module

---
+++ vkit/validatorGroup.js
@@ -0,0 +1,61 @@
+(function($){
+
+var createState = $.state;
+var valuesOf = $.valuesOf;
+var unmount = $.unmount;
+
+function not(value){
+	return !value;
+}
+
+function all(values){
+	var n = values.length;
+	for(var i=0; i<n; ++i){
+		if(!values[i]){
+			return false;
+		}
+	}
+	return true;
+}
+
+function getValidities(validators){
+	var n = validators.length;
+	var a = new Array(n);
+	for(var i=0; i<n; ++i){
+		a[i] = validators[i].valid;
+	}
+	return a;
+}
+
+function createValidatorGroup(){
+	var inputs = createState(getValidities(arguments));
+	var isValid = valuesOf(inputs).map(all);
+	
+	function add(validator){
+		var validity = validator.valid;
+		
+		inputs.set(inputs.get().concat([validity]));
+		
+		unmount(function(){
+			var validities = inputs.get();
+			for(var i=validities.length; i--;){
+				if( validities[i] === validity ){
+					inputs.set(validities.slice(0, i).concat(validities.slice(i + 1)));
+					break;
+				}
+			}
+		});
+		
+		return validator;
+	}
+	
+	return {
+		add: add,
+		valid: isValid,
+		invalid: isValid.map(not)
+	};
+}
+
+$.validatorGroup = createValidatorGroup;
+
+})($);
