d87363d66c942a7e8553c8c379f23c5165343b4f
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-05-09 21:36:40 +0200

    Update style module
    * Use dependency injection
    * Use styleContainer module
    * Use window module
    * Remove tick dependency
    * Remove DOM traversal
    * Change 'vkit-style' attribute prefix to 'vkit-'

--- vkit/style.js
+++ vkit/style.js
@@ -1,132 +1,65 @@
 (function($){
 
+var createStyleContainer = $.styleContainer;
+var getWindow = $.window;
+var inject = $.inject;
 var unmount = $.unmount;
-var tick = $.tick;
 var styleCount = 0;
-
-function getText(){
-	return this.text;
-}
-
-function updateCSS(container){
-	var cssText = container.controllersArray.join("\n");
-	if( container.textNode ){
-		container.textNode.nodeValue = cssText;
-	}else{
-		container.element.innerText = cssText;
-	}
-}
 
 function prepareCSS(css, selector){
 	return css.replace(/::?this\b/ig, selector);
 }
 
-function getRootNode(el){
-	var root = el.getRootNode ? el.getRootNode() : null;
-	if( root ){
-		return root;
+function StyleService(){
+	if(!(this instanceof StyleService)){
+		return inject(StyleService);
 	}
-	var root = el;
-	while( root.parentNode !== null ){
-		root = root.parentNode;
-	}
-	return root;
+	var document = getWindow().document;
+	var head = document.getElementsByTagName("head")[0];
+	var container = createStyleContainer(document);
+	var styleEl = container.element;
+	head.appendChild(styleEl);
+	
+	unmount(function(){
+		if( styleEl.parentNode ){
+			styleEl.parentNode.removeChild(styleEl);
+		}
+	});
+	
+	this.container = container;
+	this.document = document;
+	this.element = styleEl;
 }
 
 function createStyle(css, attr){
 	if(!attr){
-		attr = "vkit-style" + (++styleCount);
+		attr = "vkit-" + (++styleCount);
 	}
 	var selector = "[" + attr + "]";
 	
 	function bind(el){
-		var styleController = null;
+		var service = inject(StyleService);
+		var container = service.container;
+		var controller = container.add(selector);
+		controller.setValue(prepareCSS(css && typeof css.get === "function" ? css.get() : css, selector));
+		
+		if( css && typeof css.subscribe === "function" ){
+			css.subscribe(function(value){
+				controller.setValue(prepareCSS(value, selector));
+			});
+		}
 		
 		if( el.setAttribute ){
 			el.setAttribute(attr, "");
-		}
-		
-		tick(function addStyle(){
-			if(!el.parentNode && el.nodeType !== 11){
-				tick(addStyle);
-				return;
-			}
-			var doc = el.ownerDocument;
-			var root = getRootNode(el);
-			if( root !== doc && root.nodeType !== 11 ){
-				tick(addStyle);
-				return;
-			}
-			var container = root.styleContainer;
-			if(!container){
-				var head = root.getElementsByTagName ? (root.getElementsByTagName("head")[0] || root) : root;
-				var style = doc.createElement("style");
-				head.appendChild(style);
-				var textNode;
-				try{
-					textNode = doc.createTextNode("");
-					style.appendChild(textNode);
-				}catch(ex){
-					textNode = null;
-				}
-				container = root.styleContainer = {
-					root: root,
-					element: style,
-					textNode: textNode,
-					controllers: {},
-					controllersArray: []
-				};
-			}
-			var controllers = container.controllers;
-			var controller = controllers[attr];
-			if( controller ){
-				++controller.refCount;
-			}else{
-				controller = controllers[attr] = {
-					text: prepareCSS(css.get ? css.get() : css, selector),
-					toString: getText,
-					refCount: 1,
-					container: container
-				};
-				container.controllersArray.push(controller);
-				updateCSS(container);
-			}
-			styleController = controller;
-		});
-		
-		if( css.subscribe ){
-			css.subscribe(function(cssValue){
-				if( styleController ){
-					styleController.text = prepareCSS(cssValue, selector);
-					updateCSS(styleController.container);
-				}
-			});
 		}
 		
 		unmount(function(){
 			if( el.removeAttribute ){
 				el.removeAttribute(attr);
 			}
-			if( styleController && --styleController.refCount === 0 ){
-				var container = styleController.container;
-				var controllersArray = container.controllersArray;
-				for(var i=controllersArray.length; i--;){
-					if( controllersArray[i] === styleController ){
-						controllersArray.splice(i, 1);
-						break;
-					}
-				}
-				delete container.controllers[attr];
-				styleController = null;
-				for(var k in container.controllers){
-					return;
-				}
-				delete container.root.styleContainer;
-				if( container.element.parentNode ){
-					container.element.parentNode.removeChild(container.element);
-				}
-			}
-		});
+			
+			container.remove(selector);
+		});	
 	}
 	
 	bind.toString = function(){
@@ -137,5 +70,6 @@ function createStyle(css, attr){
 }
 
 $.style = createStyle;
+$.styleService = StyleService;
 
 })($);
