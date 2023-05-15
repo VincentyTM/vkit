38a916a7e381cd62b3e36bac29d89f329c3e3bc0
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-05-15 16:37:25 +0200

    Update hydrate module
    * Uses CSS query selectors
    * Works with all directives

--- vkit/hydrate.js
+++ vkit/hydrate.js
@@ -1,28 +1,41 @@
-(function($, undefined){
+(function($){
 
 var append = $.append;
+var bind = $.bind;
+var toArray = $.toArray;
 
-function hydrate(map){
-	for(var tagName in map){
-		for(var i=this.length; i--;){
-			var container = this[i];
-			if(!container.getElementsByTagName){
+function hydrate(parent, directives){
+	var n = parent.length;
+	if( typeof n === "number" && !parent.nodeType ){
+		for(var i=0; i<n; ++i){
+			hydrate(parent[i], directives);
+		}
+	}else{
+		
+		for(var selector in directives){
+			if( typeof parent.querySelectorAll !== "function" ){
 				continue;
 			}
-			var component = map[tagName];
-			var elements = container.getElementsByTagName(tagName);
-			for(var j=elements.length; j--;){
-				var element = elements[j];
-				append(element, component(element));
+			var elements = parent.querySelectorAll(selector);
+			var directive = directives[selector];
+			var m = elements.length;
+			for(var j=0; j<m; ++j){
+				var el = elements[j];
+				append(el, directive, el, bind);
 			}
-			if( container.tagName === tagName.toUpperCase() ){
-				append(container, component(container));
+			if( typeof parent.matches === "function" && parent.matches(selector) ){
+				append(parent, directive, parent, bind);
 			}
 		}
 	}
+}
+
+function hydrateThis(directives){
+	hydrate(this, directives);
 	return this;
 }
 
-$.fn.hydrate = hydrate;
+$.hydrate = hydrate;
+$.fn.hydrate = hydrateThis;
 
 })($);
