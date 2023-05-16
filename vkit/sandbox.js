de60ad5c9d990e808cb39f93b0ec59b39668484b
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-05-16 17:58:52 +0200

    Update sandbox module
    * Directive syntax
    * Using srcdoc property if available
    * Sandbox properties
    * JavaScript only (no HTML/CSS)
    * Script ending tags escaped
    * Code is stored in a local variable

--- vkit/sandbox.js
+++ vkit/sandbox.js
@@ -1,84 +1,49 @@
 (function($){
 
-$.fn.sandbox=function(){
-	var iframes=this;
-	var source="";
-	this.on("load", function(){
-		this.loaded=true;
-		var doc=this.contentWindow.document;
-		doc.body.innerHTML=source;
-		var scripts=doc.getElementsByTagName("script");
-		for(var i=0, l=scripts.length, script, parent, next, ns; i<l; ++i){
-			script=scripts[i];
-			ns=doc.createElement("script");
-			if( script.type ){
-				ns.type=script.type;
-			}
-			if( script.src ){
-				ns.src=script.src;
-			}else if( script.text ){
-				ns.text=script.text;
-			}
-			next=script.nextSibling;
-			parent=script.parentNode;
-			parent.removeChild(script);
-			parent.insertBefore(ns, next);
-		}
-	});
-	return {
-		html: function(src){
-			source=src;
-			iframes.extend({
-				"loaded": false,
-				"src": ""
-			});
-			return this;
-		},
-		run: function(src){
-			for(var i=0, l=iframes.length, iframe; i<l; ++i){
-				iframe=iframes[i];
-				if( iframe.loaded ){
-					var doc=iframe.contentWindow.document;
-					var script=doc.createElement("script");
-					script.type="text/javascript";
-					script.text=src;
-					var head=doc.getElementsByTagName("head")[0];
-					head.appendChild(script);
-					head.removeChild(script);
-				}
-			}
-			return this;
-		},
-		style: function(css){
-			var selection=$(); selection.back=selection;
-			for(var i=0, l=iframes.length, iframe, doc, head, style; i<l; ++i){
-				iframe=iframes[i];
-				if( iframe.loaded ){
-					doc=iframe.contentWindow.document;
-					head=doc.getElementsByTagName("head")[0];
-					style=doc.createElement("style");
-					style.type="text/css";
-					head.appendChild(style);
-					selection.push(style);
-					if( style.styleSheet ){
-						style.styleSheet.cssText=css;
-					}else{
-						style.appendChild( document.createTextNode(css) );
-					}
-				}
-			}
-			return selection;
-		}
-	};
-};
+var createObservable = $.observable;
+var unmount = $.unmount;
 
+function replaceScriptEnds(text){
+	return "<\\" + text.substring(1);
+}
 
-$.fn.sandboxHTML=function(html){
-	var src="data:text/html;charset=UTF-8," + encodeURIComponent('<!DOCTYPE html><html><head><meta charset="UTF-8"></head><body>' + html + '</body></html>');
-	for(var i=0, l=this.length; i<l; ++i){
-		this[i].src=src;
+function createSandbox(code, sandboxFlags){
+	if(!code) code = "";
+	var emitRun = createObservable();
+	
+	function run(newCode){
+		emitRun(code = newCode);
 	}
-	return this;
-};
+	
+	function bind(iframe){
+		function runCode(c){
+			c = c.replace(/<\/script\b/ig, replaceScriptEnds);
+			
+			var prefix = '<!DOCTYPE html><html><head><meta charset="UTF-8"></head><body><script>';
+			var suffix = '<\/script></body></html>';
+			
+			if( "srcdoc" in iframe ){
+				iframe.srcdoc = prefix + c + suffix;
+			}else{
+				iframe.src = 'data:text/html;charset=UTF-8,' + prefix + c + suffix;
+			}
+		}
+		
+		iframe.sandbox = sandboxFlags || "allow-scripts";
+		unmount(emitRun.subscribe(runCode));
+		runCode(code);
+	}
+	
+	function render(){
+		return bind;
+	}
+	
+	return {
+		run: run,
+		render: render
+	};
+}
+
+$.sandbox = createSandbox;
 
 })($);
