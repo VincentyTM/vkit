2b24cc8dd426318d481821daf7b204eff3c70997
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-05-27 19:43:31 +0200

    Add hashState replace method

--- vkit/hashState.js
+++ vkit/hashState.js
@@ -15,18 +15,28 @@ function createHashState(win){
 	if(!win) win = getWindow();
 	var location = win.location;
 	var state = createState(decodeURIComponent(location.hash.substring(1)));
-	createHistoryHandler().url().map(getHash).pipe(state);
+	var history = createHistoryHandler(win);
+	
+	history.url().map(getHash).pipe(state);
+	
 	state.onChange.subscribe(function(value){
 		location.replace("#" + encodeURIComponent(value));
 	});
+	
 	unmount(
 		onEvent(win, "hashchange", function(){
 			state.set(decodeURIComponent(location.hash.substring(1)));
 		})
 	);
+	
 	state.push = function(value){
 		location.assign("#" + encodeURIComponent(value));
 	};
+	
+	state.replace = function(value){
+		history.replace("#" + value);
+	};
+	
 	return state;
 }
 
