2a56a2380ddf7b081d837070e0c887859c85673e
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-06-08 12:43:45 +0200

    Add support for static render method

--- vkit/deepPush.js
+++ vkit/deepPush.js
@@ -4,6 +4,8 @@ var toArray = $.toArray;
 
 function deepPush(array, item, context, bind){
 	if( item === null || item === undefined || typeof item === "boolean" ){
+	}else if( typeof item.render === "function" ){
+		deepPush(array, item.render(), context, bind);
 	}else if( typeof item !== "object" ){
 		if( typeof item === "function" ){
 			item(context);
@@ -18,8 +20,6 @@ function deepPush(array, item, context, bind){
 		for(var i=0; i<n; ++i){
 			deepPush(array, a[i], context, bind);
 		}
-	}else if( typeof item.render === "function" ){
-		deepPush(array, item.render(), context, bind);
 	}else if( typeof item.next === "function" ){
 		var x;
 		do{
