967843f9f5a348394d2a51c64e28cddeef31fcf4
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-06-28 16:50:24 +0200

    Use current window in visibilityState

--- vkit/visibilityState.js
+++ vkit/visibilityState.js
@@ -1,21 +1,30 @@
-(function($, document){
+(function($){
 
 var createState = $.state;
-var unmount = $.unmount;
+var getWindow = $.window;
 var onEvent = $.onEvent;
+var unmount = $.unmount;
 
 function createVisibilityState(doc){
 	if(!doc){
-		doc = document;
+		doc = getWindow().document;
 	}
+	
 	function onChange(){
 		visibility.set(doc.visibilityState === "visible");
 	}
-	var visibility = createState(doc.visibilityState === "visible" || !doc.visibilityState);
-	unmount( onEvent(doc, "visibilitychange", onChange) );
+	
+	var visibility = createState(
+		doc.visibilityState === "visible" || !doc.visibilityState
+	);
+	
+	unmount(
+		onEvent(doc, "visibilitychange", onChange)
+	);
+	
 	return visibility;
 }
 
 $.visibilityState = createVisibilityState;
 
-})($, document);
+})($);
