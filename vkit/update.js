e9f83a9d33a681bae85ff0e25d42350e350427a9
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-07-08 12:52:34 +0200

    Create update module

---
+++ vkit/update.js
@@ -0,0 +1,50 @@
+(function($, g){
+
+var callTicks = $.callTicks;
+var rootComponent = $.rootComponent;
+var queue = [];
+
+var queueMicrotask = g.queueMicrotask || (typeof Promise === "function" && typeof Promise.resolve === "function"
+	? function(callback){ Promise.resolve().then(callback); }
+	: function(callback){ setTimeout(callback, 0); });
+
+function enqueueUpdate(update){
+	queue.push(update);
+	
+	if( queue.length === 1 ){
+		queueMicrotask(update);
+	}
+}
+
+function dequeueUpdate(update){
+	for(var i=queue.length; i--;){
+		if( queue[i] === update ){
+			queue.splice(i, 1);
+			break;
+		}
+	}
+}
+
+function update(){
+	var n;
+	
+	do{
+		while( n = queue.length ){
+			var updates = queue.splice(0, n);
+			
+			for(var i=0; i<n; ++i){
+				updates[i]();
+			}
+		}
+		
+		rootComponent.render();
+	}while(n);
+	
+	callTicks();
+}
+
+$.dequeueUpdate = dequeueUpdate;
+$.enqueueUpdate = enqueueUpdate;
+$.update = update;
+
+})($, this);
