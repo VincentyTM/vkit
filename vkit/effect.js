427a306b5be082219e10929e3095463cca2712b8
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-07-19 13:18:31 +0200

    Add component-based effect

--- vkit/effect.js
+++ vkit/effect.js
@@ -1,7 +1,9 @@
 (function($){
 
-function createEffect(){
-	throw new Error("Effect method is not available");
+var createComponent = $.component;
+
+function createEffect(callback){
+	createComponent(callback).render();
 }
 
 $.effect = createEffect;
