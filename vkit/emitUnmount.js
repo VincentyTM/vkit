feb8bcdfb068271c913663649b25a378c33da350
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-07-20 15:47:38 +0200

    Remove unmounted components from children array

--- vkit/emitUnmount.js
+++ vkit/emitUnmount.js
@@ -3,13 +3,27 @@
 function emitUnmount(component){
 	var children = component.children;
 	if( children ){
+		component.children = null;
+		
 		var n = children.length;
 		
 		for(var i=0; i<n; ++i){
 			emitUnmount(children[i]);
 		}
+	}
+	
+	var parent = component.parent;
+	if( parent ){
+		var siblings = parent.children;
 		
-		component.children = null;
+		if( siblings ){
+			for(var i=siblings.length; i--;){
+				if( siblings[i] === component ){
+					siblings.splice(i, 1);
+					break;
+				}
+			}
+		}
 	}
 	
 	var unmount = component.unmount;
