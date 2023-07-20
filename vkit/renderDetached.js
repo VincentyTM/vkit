e65abe9ffff2629966ceee0eb74e4f94574a4c21
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-07-20 13:28:50 +0200

    Use rootProvider in renderDetached

--- vkit/renderDetached.js
+++ vkit/renderDetached.js
@@ -6,35 +6,34 @@ var createComponent = $.component;
 var emitUnmount = $.emitUnmount;
 var inject = $.inject;
 var provide = $.provide;
+var rootProvider = $.rootProvider;
 var update = $.update;
 var WindowService = $.windowService;
 
 function renderDetached(getView, parent){
 	var component = createComponent(function(){
-		provide(null, function(){
-			var win = null;
+		var win = null;
+		
+		if( parent ){
+			var doc = parent.ownerDocument;
 			
-			if( parent ){
-				var doc = parent.ownerDocument;
-				
-				if( doc ){
-					win = doc.defaultView || doc.parentWindow;
-				}
+			if( doc ){
+				win = doc.defaultView || doc.parentWindow;
 			}
-			
-			if( win ){
-				inject(WindowService).window = win;
-			}
-			
-			var view = getView(function(){
-				emitUnmount(component);
-			}, component);
-			
-			if( parent ){
-				append(parent, view, parent, bind);
-			}
-		});
-	}, null, null);
+		}
+		
+		if( win ){
+			inject(WindowService).window = win;
+		}
+		
+		var view = getView(function(){
+			emitUnmount(component);
+		}, component);
+		
+		if( parent ){
+			append(parent, view, parent, bind);
+		}
+	}, null, rootProvider);
 	
 	component.render();
 	update();
