e4c387fbc8c0ae14ebc151a44002da35fdc45999
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-07-26 23:49:20 +0200

    Add WindowService context and data

--- vkit/window.js
+++ vkit/window.js
@@ -3,11 +3,14 @@
 var bind = $.bind;
 var getComponent = $.getComponent;
 var getProvider = $.getProvider;
+var getContext = $.context;
 var inject = $.inject;
 
 function WindowService(){
 	this.component = getComponent();
 	this.provider = getProvider();
+	this.context = getContext();
+	this.data = null;
 	this.window = window;
 }
 
