b7e23df7ee66a452ecc465ced218291a969075a8
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-07-26 23:54:28 +0200

    Allow renderable functions in server deepPush

--- vkit-server/deepPush.js
+++ vkit-server/deepPush.js
@@ -3,6 +3,8 @@ var toArray = require("./toArray.js");
 
 function deepPush(array, item, context, bind){
 	if( item === null || item === undefined || typeof item === "boolean" ){
+	}else if( typeof item.render === "function" ){
+		deepPush(array, item.render(), context, bind);
 	}else if( typeof item !== "object" ){
 		if( typeof item === "function" ){
 			item(context);
@@ -17,8 +19,6 @@ function deepPush(array, item, context, bind){
 		for(var i=0; i<n; ++i){
 			deepPush(array, a[i], context, bind);
 		}
-	}else if( typeof item.render === "function" ){
-		deepPush(array, item.render(), context, bind);
 	}else if( typeof item.next === "function" ){
 		var x;
 		do{
