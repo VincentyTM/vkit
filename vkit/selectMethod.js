5a87736507d23aa72fb5e453c6f4bf573bc6ec44
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-07-29 22:34:09 +0200

    Create selectMethod module

---
+++ vkit/selectMethod.js
@@ -0,0 +1,42 @@
+(function($){
+
+var computed = $.computed;
+var isArray = $.isArray;
+var selectProperty = $.selectProperty;
+
+function selectMethod(parent, name, args, dependencies){
+	var signal = computed(function(){
+		var value = parent.get();
+		return value && value[name].apply(value, arguments);
+	}, args);
+	
+	var m = dependencies.length;
+	var deps = new Array(m);
+	
+	for(var i=0; i<m; ++i){
+		var dep = dependencies[i];
+		
+		if( typeof dep === "string" ){
+			dep = selectProperty(parent, dep);
+		}else if( isArray(dep) ){
+			var arr = dep;
+			var l = arr.length;
+			
+			dep = parent;
+			
+			for(var j=0; j<l; ++j){
+				dep = selectProperty(dep, arr[j]);
+			}
+		}else{
+			throw new Error("Invalid dependency " + dep);
+		}
+		
+		dep.subscribe(signal.update);
+	}
+	
+	return signal;
+};
+
+$.selectMethod = selectMethod;
+
+})($);
