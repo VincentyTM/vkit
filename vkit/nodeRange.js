8b90ae67e7839b1c1f3b4ae5a9c410e52f63a7cf
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-07-29 17:36:04 +0200

    Merge nodeRange getNodes and render methods

--- vkit/nodeRange.js
+++ vkit/nodeRange.js
@@ -7,15 +7,13 @@ function createNodeRange(){
 	var start = document.createTextNode("");
 	var end = document.createTextNode("");
 	
-	function render(){
-		return [start, end];
-	}
-	
 	function clear(){
 		if(!start.nextSibling){
 			throw new Error("Cannot clear detached range");
 		}
+		
 		var parent = start.parentNode;
+		
 		if( parent ){
 			for(var el = end.previousSibling; el && el !== start; el = end.previousSibling){
 				parent.removeChild(el);
@@ -33,6 +31,7 @@ function createNodeRange(){
 		if(!start.nextSibling){
 			throw new Error("Cannot append to detached range");
 		}
+		
 		insert(arguments, end);
 	}
 	
@@ -40,42 +39,49 @@ function createNodeRange(){
 		if(!start.nextSibling){
 			throw new Error("Cannot insert detached range");
 		}
+		
 		var parent = anchor.parentNode;
+		
 		if( parent ){
 			var el = start;
+			
 			while( el && el !== end ){
 				var next = el.nextSibling;
 				parent.insertBefore(el, anchor);
 				el = next;
 			}
+			
 			parent.insertBefore(end, anchor);
 		}
 	}
 	
-	function getNodes(){
+	function render(){
 		if(!start.nextSibling){
-			throw new Error("Cannot get nodes of detached range");
+			throw new Error("Cannot render detached range");
 		}
+		
 		var nodes = [];
 		var parent = start.parentNode;
+		
 		if( parent ){
 			for(var el = start; el && el !== end; el = el.nextSibling){
 				nodes.push(el);
 			}
+			
 			nodes.push(end);
 		}
+		
 		return nodes;
 	}
 	
 	return {
-		render: render,
-		start: start,
-		end: end,
-		clear: clear,
-		remove: remove,
 		append: append,
+		clear: clear,
+		end: end,
 		insertBefore: insertBefore,
-		getNodes: getNodes
+		remove: remove,
+		render: render,
+		start: start
 	};
 }
 
