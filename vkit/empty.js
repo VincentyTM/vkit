e060971e1c2c33d0d1710ad8947b671b5a6b186f
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-08-19 18:41:50 +0200

    Remove $.fn.empty

--- vkit/empty.js
+++ vkit/empty.js
@@ -5,20 +5,13 @@ function empty(parent){
 		parent.replaceChildren();
 	}else{
 		var child;
+		
 		while( child = parent.lastChild ){
 			parent.removeChild(child);
 		}
 	}
 }
 
-function emptyAll(){
-	for(var i=this.length; i--;){
-		empty(this[i]);
-	}
-	return this;
-}
-
 $.empty = empty;
-$.fn.empty = emptyAll;
 
 })($);
