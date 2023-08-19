752a141f7d08facb3f2b44ad52075ac4e3971f7c
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-08-19 18:42:33 +0200

    Update code style in currentScript

--- vkit/currentScript.js
+++ vkit/currentScript.js
@@ -1,20 +1,27 @@
 (function($, document){
 
-function getCurrentScript(doc){
-	if(!doc) doc = document;
+function currentScript(doc){
+	if(!doc){
+		doc = document;
+	}
+	
 	if( doc.currentScript ){
 		return doc.currentScript;
 	}
+	
 	var scripts = doc.scripts;
-	for(var i=scripts.length-1; i>=0; --i){
-		var script=scripts[i];
-		if(!script.readyState || script.readyState=="interactive"){
+	
+	for(var i = scripts.length-1; i >= 0; --i){
+		var script = scripts[i];
+		
+		if(!script.readyState || script.readyState === "interactive"){
 			return script;
 		}
 	}
+	
 	throw new ReferenceError("Current script not found!");
 }
 
-$.currentScript = getCurrentScript;
+$.currentScript = currentScript;
 
 })($, document);
