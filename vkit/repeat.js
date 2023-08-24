a13e36a9df1146cd4f06e9d50c43c507be951084
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-08-24 09:17:35 +0200

    Allow using repeat with signals

--- vkit/repeat.js
+++ vkit/repeat.js
@@ -6,28 +6,37 @@ function getNumber(num){
 	if( typeof num !== "number" ){
 		throw new TypeError("Repeat count can only be a number");
 	}
+	
 	return !(num >= 0) ? 0 : num >= MAX_COUNT ? MAX_COUNT : Math.floor(num);
 }
 
 function createRangeArray(length){
 	length = getNumber(length);
+	
 	var array = new Array(length);
+	
 	for(var i=0; i<length; ++i){
 		array[i] = i;
 	}
+	
 	return array;
 }
 
 function repeat(count, getView){
-	if( typeof count === "object" ){
+	if( count && typeof count.map === "function" ){
 		var arrayState = count.map(createRangeArray);
+		
 		return arrayState.views(getView);
 	}
+	
 	count = getNumber(count);
+	
 	var array = new Array(count);
+	
 	for(var i=0; i<count; ++i){
 		array[i] = getView(i);
 	}
+	
 	return array;
 }
 
