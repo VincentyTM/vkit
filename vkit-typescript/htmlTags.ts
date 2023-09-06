8574b96c1f7f374ecf3e3a15b4d53bb35e962ee8
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-09-06 09:30:49 +0200

    Add htmlTags missing types

--- vkit-typescript/htmlTags.ts
+++ vkit-typescript/htmlTags.ts
@@ -3,7 +3,7 @@ import htmlTag from "./htmlTag";
 import type {View} from "./view";
 
 var htmlTags = new Proxy({} as {[key: string]: (...contents: View[]) => HTMLElement}, {
-    get: function(_target, tagName: string, _receiver){
+    get: function(_target: object, tagName: string, _receiver: object){
         return htmlTag(tagName.toLowerCase().replace(/_/g, "-"));
     }
 });
