b0bf8bfcd72a55851dad50518b39407f2ee65850
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-09-17 18:51:26 +0200

    Use directive in deepPush

--- vkit/deepPush.js
+++ vkit/deepPush.js
@@ -1,34 +1,56 @@
-(function($, document, undefined){
+(function($, document, undefined) {
 
+var directive = $.directive;
 var toArray = $.toArray;
 
-function deepPush(array, item, context, bind){
-	if( item === null || item === undefined || typeof item === "boolean" ){
-	}else if( typeof item.render === "function" ){
+function deepPush(array, item, context, bind) {
+	if (item === null || item === undefined || item === false || item === true){
+		return array;
+	}
+	
+	if (typeof item.render === "function") {
 		deepPush(array, item.render(), context, bind);
-	}else if( typeof item !== "object" ){
-		if( typeof item === "function" ){
-			item(context);
-		}else{
-			array.push(document.createTextNode(item));
-		}
-	}else if( item.nodeType ){
+		return array;
+	}
+	
+	if (typeof item === "function") {
+		deepPush(array, directive(context, item), context, bind);
+		return array;
+	}
+	
+	if (typeof item !== "object") {
+		array.push(document.createTextNode(item));
+		return array;
+	}
+	
+	if (item.nodeType) {
 		array.push(item);
-	}else if( typeof item.length === "number" ){
+		return array;
+	}
+	
+	if (typeof item.length === "number") {
 		var n = item.length;
 		var a = toArray(item);
-		for(var i=0; i<n; ++i){
+		for (var i = 0; i < n; ++i) {
 			deepPush(array, a[i], context, bind);
 		}
-	}else if( typeof item.next === "function" ){
+		return array;
+	}
+	
+	if (typeof item.next === "function") {
 		var x;
-		do{
+		do {
 			x = item.next();
 			deepPush(array, x.value, context, bind);
-		}while(!x.done);
-	}else if( bind ){
-		bind(context, item, true);
+		} while (!x.done);
+		return array;
 	}
+	
+	if (bind) {
+		bind(context, item, true);
+		return array;
+	}
+	
 	return array;
 }
 
