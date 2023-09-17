54a78b31add199b9dd0fc6748962e27539d2561b
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-09-17 18:42:11 +0200

    Add contextGuard function overloading

--- vkit-typescript/contextGuard.ts
+++ vkit-typescript/contextGuard.ts
@@ -5,11 +5,14 @@ var currentComponent: Component | null = null;
 var currentInjector: Injector | null = null;
 var errorMessage = "This function can only be called synchronously from a component";
 
+export function getComponent(): Component;
+
+export function getComponent(allowNull: true): Component | null;
+
 export function getComponent(allowNull?: boolean): Component | null {
 	if (!allowNull && !currentComponent) {
 		throw new Error(errorMessage);
 	}
-	
 	return currentComponent;
 }
 
@@ -17,11 +20,14 @@ export function setComponent(component: Component | null): void {
 	currentComponent = component;
 }
 
+export function getInjector(): Injector;
+
+export function getInjector(allowNull: true): Injector | null;
+
 export function getInjector(allowNull?: boolean): Injector | null {
 	if (!allowNull && (!currentComponent || !currentInjector)) {
 		throw new Error(errorMessage);
 	}
-	
 	return currentInjector;
 }
 
