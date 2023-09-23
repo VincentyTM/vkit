408b5ea054082e59a24f15f10ba4b59301486a4e
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-09-23 22:17:38 +0200

    Update replaceScript
    * Create a view in the root component
    * Search context element the same way as in the html module

--- vkit/replaceScript.js
+++ vkit/replaceScript.js
@@ -1,18 +1,48 @@
 (function($){
 
+var createView = $.view;
 var currentScript = $.currentScript;
 var insert = $.insert;
 var remove = $.remove;
-var render = $.render;
+var rootComponent = $.rootComponent;
+var rootInjector = $.rootInjector;
+var setComponent = $.setComponent;
+var setInjector = $.setInjector;
+var update = $.update;
 
 function replaceScript(getView){
 	var script = currentScript();
-	var view = typeof getView === "function" ? render(getView) : getView;
 	
-	insert(view, script);
-	remove(script);
+	try{
+		setComponent(rootComponent);
+		setInjector(rootInjector);
+		
+		var view = typeof getView === "function" ? createView(getView) : getView;
+		var context = script.previousElementSibling;
+		
+		if (context === undefined) {
+			context = script;
+			
+			while (context = context.previousSibling) {
+				if (context.nodeType === 1) {
+					break;
+				}
+			}
+		}
+		
+		if (!context) {
+			context = script.parentNode;
+		}
+		
+		insert(view, script, context);
+		remove(script);
+	}finally{
+		setComponent(null);
+		setInjector(null);
+	}
 	
-	return view;
+	update();
+	return rootComponent;
 }
 
 $.replaceScript = replaceScript;
