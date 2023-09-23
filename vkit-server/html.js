23b82485e93354925be166807a1f0732abbe682f
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-09-23 20:51:53 +0200

    Update code style in server modules

--- vkit-server/html.js
+++ vkit-server/html.js
@@ -1,24 +1,24 @@
 var deepPush = require("./deepPush.js");
 var isArray = require("./isArray.js");
 
-function createHTML(html){
+function createHTML(html) {
 	return {
-		toHTML: function(res){
+		toHTML: function(res) {
 			res.write(html);
 		}
 	};
 }
 
-function html(strings){
-	if( isArray(strings) && isArray(strings.raw) ){
+function html(strings) {
+	if (isArray(strings) && isArray(strings.raw)) {
 		var n = strings.length;
 		var a = new Array(2*n - 1);
 		
-		if( n > 0 ){
+		if (n > 0) {
 			a[0] = strings[0];
 		}
 		
-		for(var i = 1, j = 1; i < n; ++i){
+		for (var i = 1, j = 1; i < n; ++i) {
 			var arg = arguments[i];
 			a[j++] = typeof arg === "string" ? [arg] : arg;
 			a[j++] = strings[i];
@@ -30,32 +30,29 @@ function html(strings){
 	var parts = [];
 	var n = arguments.length;
 	
-	for(var i=0; i<n; ++i){
+	for (var i = 0; i < n; ++i) {
 		var arg = arguments[i];
 		
-		while( arg && typeof arg.render === "function" ){
+		while (arg && typeof arg.render === "function") {
 			arg = arg.render();
 		}
 		
-		switch(typeof arg){
+		switch (typeof arg) {
 			case "string":
 			case "number":
 			case "bigint":
 			case "symbol":
 				parts.push(createHTML(String(arg)));
 				break;
-			
 			case "object":
-				if( arg.toHTML ){
+				if (arg.toHTML) {
 					parts.push(arg);
-				}else if( isArray(arg) ){
+				} else if (isArray(arg)) {
 					deepPush(parts, arg);
-				}else{
+				} else {
 					throw new Error("Bindings in html are not allowed on the server");
 				}
-				
 				break;
-			
 			case "function":
 			case "undefined":
 			case "boolean":
