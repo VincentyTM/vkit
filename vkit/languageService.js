71c8f2094c0af2a7e8aee130fa1a256195ae3f07
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-10-14 20:39:10 +0200

    Update languageService
    * Allow multi-digit word parameters
    * Compute word immediately
    * Throw if word argument count is not equal to %parameter count in key
    * Throw if word contains unused %parameters
    * Update code style

--- vkit/languageService.js
+++ vkit/languageService.js
@@ -1,38 +1,39 @@
-(function($, undefined){
+(function($, undefined) {
 
 var computed = $.computed;
-var createState = $.state;
 var inject = $.inject;
+var isSignal = $.isSignal;
+var signal = $.signal;
 var slice = Array.prototype.slice;
 
-var assign = Object.assign || function(a, b){
-	for(var k in b){
+var assign = Object.assign || function(a, b) {
+	for (var k in b) {
 		a[k] = b[k];
 	}
 };
 
-function selectWordsOfLang(dictionary, code){
+function selectWordsOfLang(dictionary, code) {
 	return code ? dictionary[code] : null;
 }
 
-function LanguageService(){
-	if(!(this instanceof LanguageService)){
+function LanguageService() {
+	if (!(this instanceof LanguageService)) {
 		return inject(LanguageService);
 	}
 	
-	var langCode = createState(null);
-	var dictionary = createState({});
+	var langCode = signal(null);
+	var dictionary = signal({});
 	var wordsOfLang = computed(selectWordsOfLang, [dictionary, langCode]);
 	
-	function define(update){
+	function define(update) {
 		var newDict = {};
 		var dict = dictionary.get();
 		
 		assign(newDict, dict);
 		
-		for(var code in update){
+		for (var code in update) {
 			var words = newDict[code] = {};
-			if( dict[code] ){
+			if (dict[code]) {
 				assign(words, dict[code]);
 			}
 			assign(words, update[code]);
@@ -42,51 +43,59 @@ function LanguageService(){
 		wordsOfLang.update();
 	}
 	
-	function createWord(key){
-		var keyState = key && typeof key.get === "function" ? key : {get: function(){ return key; }};
-		var args = arguments;
+	function createWord(key) {
+		var args = slice.call(arguments, 1);
 		var n = args.length;
-		var states = [wordsOfLang, keyState];
-		
-		for(var i=1; i<n; ++i){
-			if( args[i] === undefined ){
-				throw new Error("Argument %" + i + " missing from word " + key);
-			}
-			if( args[i].get ){
-				states.push(args[i]);
-			}
-		}
-		
-		return computed(function(lang, key){
-			if(!lang){
+		var c = computed(function(lang, key) {
+			if (!lang) {
 				var code = langCode.get();
 				throw new Error(code ? "Language '" + code + "' does not exist" : "No language has been set");
 			}
+			
 			var word = lang[key];
-			if(!key){
+			
+			if (!key) {
 				return "";
 			}
-			switch(typeof word){
+			
+			var argCount = key.split(/%\d+/).length - 1;
+			
+			if (argCount !== n) {
+				throw new Error("Argument count must be " + argCount + " (instead of " + n + ") in word " + key);
+			}
+			
+			switch (typeof word) {
 				case "function":
-					var values = slice.call(args, 1);
-					var m = n - 1;
-					for(var i=0; i<m; ++i){
-						var value = values[i];
-						if( value && value.get ){
-							values[i] = value.get();
-						}
+					var values = new Array(n);
+					
+					for (var i = 0; i < n; ++i) {
+						var arg = args[i];
+						values[i] = isSignal(arg) ? arg.get() : arg;
 					}
+					
 					return word.apply(null, values);
 				case "string":
-					for(var i=1; i<n; ++i){
+					for (var i = 0; i < n; ++i) {
 						var arg = args[i];
-						word = word.replace("%" + i, arg && arg.get ? arg.get() : arg);
+						word = word.replace(new RegExp("%" + (i + 1) + "(?!\d)"), isSignal(arg) ? arg.get() : arg);
 					}
+					
+					if (/%\d/.test(word)) {
+						throw new Error("Argument value not specified in word " + word);
+					}
+					
 					return word;
 				default:
 					throw new Error("Undefined word " + key);
 			}
-		}, states);
+		}, [
+			wordsOfLang,
+			isSignal(key) || typeof key !== "function" ? key : computed(key)
+		].concat(args));
+		
+		c.update();
+		
+		return c;
 	}
 	
 	this.define = define;
@@ -94,15 +103,15 @@ function LanguageService(){
 	this.word = createWord;
 }
 
-function define(dictionary){
+function define(dictionary) {
 	return inject(LanguageService).define(dictionary);
 }
 
-function createWord(){
+function createWord() {
 	return inject(LanguageService).word.apply(null, arguments);
 }
 
-function langCode(){
+function langCode() {
 	return inject(LanguageService).lang;
 }
 
