629c86deaa61a88c360e44b5dc2fc2fe25ff1f7b
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-10-14 20:27:44 +0200

    Add of description

--- vkit-typescript/of.ts
+++ vkit-typescript/of.ts
@@ -33,6 +33,22 @@ var handler = {
 	get: getValue
 };
 
+/**
+ * Creates and returns a proxy which adds reactivity to an object's properties.
+ * @example
+ * const object = {count: 0};
+ * const count = computed(() => of(object).count);
+ * console.log(count.get()); // 0
+ * 
+ * ++object.count;
+ * console.log(count.get()); // 0
+ * 
+ * update();
+ * console.log(count.get()); // 1
+ * 
+ * @param object The object whose properties need to be observed.
+ * @returns The proxy of the object.
+ */
 export default function of<ObjectType extends Object>(object: ObjectType) {
 	return new Proxy<ObjectType>(object, handler);
 }
