222c7d007be770ea7d04d10cc63e6bc1e20946ac
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-10-14 20:24:44 +0200

    Add effect description

--- vkit-typescript/effect.ts
+++ vkit-typescript/effect.ts
@@ -1,5 +1,24 @@
 import component from "./component";
 
+/**
+ * Creates a side effect and runs it every time its dependencies change.
+ * @example
+ * const shouldRunInterval = signal(true);
+ * 
+ * effect(() => {
+ * 	if (!shouldRunInterval()) {
+ * 		return;
+ * 	}
+ * 	
+ * 	const interval = setInterval(() => {
+ * 		console.log("The current time is:", Date.now());
+ * 	}, 1000);
+ * 	
+ * 	onUnmount(() => clearInterval(interval));
+ * });
+ * @param callback A callback function which is called initially and when any of its dependencies change.
+ * In order to clean up side effects, call onUnmount within the callback.
+ */
 export default function effect(callback: () => void): void {
 	component(callback).render();
 }
