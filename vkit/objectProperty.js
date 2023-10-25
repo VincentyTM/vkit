ecc6a0e87b9ab5ebe55eed5ce4ec111c13ecc04d
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-10-25 22:25:56 +0200

    Allow null/undefined object in objectProperty

--- vkit/objectProperty.js
+++ vkit/objectProperty.js
@@ -7,8 +7,24 @@ var observe = $.observe;
 var onUnmount = $.onUnmount;
 var signal = $.signal;
 
+function getDefaultValue(defaultValue, prop) {
+	var type = typeof defaultValue;
+	
+	if (type === "function") {
+		return defaultValue();
+	}
+	
+	if (type === "string" || type === "number" || type === "boolean" || type === "bigint" || defaultValue === null) {
+		return defaultValue;
+	}
+	
+	throw new Error("Property '" + prop + "' does not exist and there is no default value provided");
+}
+
 function objectProperty(object, property, defaultValue) {
-	var value = signal(get(object)[get(property)]);
+	var obj = get(object);
+	var prop = get(property);
+	var value = signal(obj ? obj[prop] : getDefaultValue(defaultValue, prop));
 	var setEagerly = value.setEagerly;
 	
 	value.subscribe(function(v) {
@@ -18,17 +34,16 @@ function objectProperty(object, property, defaultValue) {
 	effect(function() {
 		var o = isSignal(object) ? object() : object;
 		var p = isSignal(property) ? property() : property;
+		
+		if (o === null || o === undefined) {
+			setEagerly(getDefaultValue(defaultValue, p));
+			return;
+		}
+		
 		var change = observe(o, p);
 		
 		if (!change) {
-			var type = typeof defaultValue;
-			if (type === "function") {
-				o[p] = defaultValue();
-			} else if (type === "string" || type === "number" || type === "boolean" || type === "bigint" || defaultValue === null) {
-				o[p] = defaultValue;
-			} else {
-				throw new Error("Property '" + p + "' does not exist and there is no default value provided");
-			}
+			o[p] = getDefaultValue(defaultValue, p);
 			change = observe(o, p);
 		}
 		
