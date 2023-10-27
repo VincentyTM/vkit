74477630f62205b9952df3e91c138c8c13541276
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-10-27 19:01:14 +0200

    Add TypeScript htmlTag description

--- vkit-typescript/htmlTag.ts
+++ vkit-typescript/htmlTag.ts
@@ -2,6 +2,35 @@ import append from "./append";
 import bind from "./bind";
 import {View} from "./view";
 
+/**
+ * Creates and returns an HTML tag (element factory).
+ * In order to create a custom HTML element, you may use underscores instead of dashes.
+ * For a shorter syntax see `htmlTags`.
+ * @example
+ * const Button = htmlTag("button");
+ * const H1 = htmlTag("h1");
+ * const Input = htmlTag("input");
+ * const My_Custom_Element = htmlTag("my-custom-element");
+ * 
+ * function Component() {
+ * 	return My_Custom_Element(
+ * 		H1("Hello world", {
+ * 			style: {
+ * 				color: "red"
+ * 			}
+ * 		}),
+ * 		Input({
+ * 			readOnly: true,
+ * 			value: "A read-only input field"
+ * 		}),
+ * 		Button("Click here", {
+ * 			onclick() {
+ * 				console.log("A clickable button!");
+ * 			}
+ * 		})
+ * 	);
+ * }
+ */
 export default function htmlTag<K extends keyof HTMLElementTagNameMap>(tagName: K): (
 	...contents: View<HTMLElementTagNameMap[K]>[]
 ) => HTMLElementTagNameMap[K] {
