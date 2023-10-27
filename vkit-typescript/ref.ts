35318552fac9c6cad641b246ec69a6a9591faca4
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2023-10-27 19:09:51 +0200

    Add TypeScript ref description

--- vkit-typescript/ref.ts
+++ vkit-typescript/ref.ts
@@ -5,6 +5,26 @@ type Ref<ValueType> = {
 	current: ValueType | null;
 };
 
+/**
+ * Creates and returns a reference which can contain any object (usually an HTML element) or null.
+ * You can get its current value with its `current` property.
+ * @example
+ * function MyComponent() {
+ * 	const input = ref();
+ * 	
+ * 	return [
+ * 		Input(input),
+ * 		Button("Focus", {
+ * 			onclick() {
+ * 				if (input.current) {
+ * 					input.current.focus();
+ * 				}
+ * 			}
+ * 		})
+ * 	];
+ * }
+ * @returns A function directive which binds an element (or any other object) to the reference until the current component unmounts.
+ */
 export default function createRef<ValueType extends object = HTMLElement>() {
 	function reset(): void {
 		ref.current = null;
