c7b3c84d08eee3ad439f5b4034acc8f4c554eb7f
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-02-18 23:32:43 +0100

    Remove binding parameters from getWindow

--- vkit-typescript/src/getWindow.ts
+++ vkit-typescript/src/getWindow.ts
@@ -1,4 +1,3 @@
-import bind from "./bind";
 import getContext from "./context";
 import inject from "./inject";
 import {View} from "./view";
@@ -17,12 +16,5 @@ export var WindowService = function(this: WindowService) {
 } as unknown as WindowService;
 
 export default function getWindow() {
-	var win = inject(WindowService).window;
-	var n = arguments.length;
-
-	for (var i = 0; i < n; ++i) {
-		bind(win, arguments[i]);
-	}
-	
-	return win;
+	return inject(WindowService).window;
 }
