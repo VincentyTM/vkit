9a65703dfdb91bce7428908a646ffaed9ef6ff8d
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-02-18 20:53:02 +0100

    Allow function condition in ifElse

--- vkit/ifElse.js
+++ vkit/ifElse.js
@@ -1,16 +1,18 @@
-(function($){
+(function($) {
 
-function ifElseSlice(args, offset, argsCount){
-	if( argsCount < 2 ){
+var signalFrom = $.signalFrom;
+
+function ifElseSlice(args, offset, argsCount) {
+	if (argsCount < 2) {
 		return argsCount === 1 ? args[offset]() : null;
 	}
 	
-	return args[offset].map(Boolean).view(function(value){
+	return signalFrom(args[offset]).map(Boolean).view(function(value) {
 		return value ? args[offset + 1]() : ifElseSlice(args, offset + 2, argsCount - 2);
 	});
 }
 
-function ifElse(){
+function ifElse() {
 	return ifElseSlice(arguments, 0, arguments.length);
 }
 
