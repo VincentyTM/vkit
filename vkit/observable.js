e76ad1d05c5648c60ecae9d79177e7ecd2df7041
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-02-18 23:56:02 +0100

    Update code style in observable

--- vkit/observable.js
+++ vkit/observable.js
@@ -1,22 +1,22 @@
-(function($){
+(function($) {
 
-function createObservable(){
+function createObservable() {
 	var callbacks = [], n = 0;
 	
-	function observable(){
-		for(var i = 0; i < n; i += 2){
+	function observable() {
+		for (var i = 0; i < n; i += 2) {
 			callbacks[i].apply(this, arguments);
 		}
 	}
 	
-	function subscribe(callback){
-		if( typeof callback !== "function" ){
+	function subscribe(callback) {
+		if (typeof callback !== "function") {
 			throw new Error("Callback is not a function");
 		}
 		
-		function unsubscribe(){
-			for(var i = Math.min(curr, n) - 1; i > 0; i -= 2){
-				if( callbacks[i] === unsubscribe ){
+		function unsubscribe() {
+			for (var i = Math.min(curr, n) - 1; i > 0; i -= 2) {
+				if (callbacks[i] === unsubscribe) {
 					callbacks.splice(i - 1, 2);
 					n -= 2;
 					break;
@@ -29,20 +29,20 @@ function createObservable(){
 		return unsubscribe;
 	}
 	
-	function getCount(){
+	function getCount() {
 		return n / 2;
 	}
 	
-	function clear(){
-		if( n > 0 ){
+	function clear() {
+		if (n > 0) {
 			callbacks.splice(0, n);
 			n = 0;
 		}
 	}
 	
-	function has(callback){
-		for(var i = n - 2; i >= 0; i -= 2){
-			if( callbacks[i] === callback ){
+	function has(callback) {
+		for (var i = n - 2; i >= 0; i -= 2) {
+			if (callbacks[i] === callback) {
 				return true;
 			}
 		}
