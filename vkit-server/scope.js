25dc929da389c87957a3a33b92e92dd5b8e53f6a
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-02-18 21:07:18 +0100

    Update code style in server scope

--- vkit-server/scope.js
+++ vkit-server/scope.js
@@ -1,51 +1,51 @@
 var escapeHTML = require("./escapeHTML");
 var scope = null;
 
-function replaceStyleEnds(text){
+function replaceStyleEnds(text) {
 	return '<\\/' + text.substring(2);
 }
 
-function createScope(req, res){
+function createScope(req, res) {
 	var styles = [];
 	var styleCount = 0;
 	var styleKeys = {};
 	var windowData = {};
 	
-	function addStyle(key, css){
-		if(!(key in styleKeys)){
+	function addStyle(key, css) {
+		if (!(key in styleKeys)) {
 			styleKeys[key] = true;
 			styles.push(css);
 		}
 	}
 	
-	function addWindowData(name, value){
-		if( windowData[name] ){
+	function addWindowData(name, value) {
+		if (windowData[name]) {
 			windowData[name].push(value);
-		}else{
+		} else {
 			windowData[name] = [value];
 		}
 	}
 	
-	function getStyles(){
+	function getStyles() {
 		return styles.join('\n').replace(/<\/style\b/ig, replaceStyleEnds);
 	}
 	
-	function getWindowData(name, value){
+	function getWindowData(name, value) {
 		var parts = windowData[name];
 		
-		if( parts ){
+		if (parts) {
 			var n = parts.length;
 			
-			for(var i=0; i<n; ++i){
+			for (var i = 0; i < n; ++i) {
 				var part = parts[i];
 				
-				if( part && typeof part.get === "function" ){
+				if (part && typeof part.get === "function") {
 					part = part.get();
 				}
 				
-				if( typeof part === "function" ){
+				if (typeof part === "function") {
 					value = part(value);
-				}else{
+				} else {
 					value = part;
 				}
 			}
@@ -56,9 +56,9 @@ function createScope(req, res){
 	
 	function meta(){
 		return {
-			toHTML: function(res){
-				for(var key in windowData){
-					if( key.indexOf("meta:") === 0 ){
+			toHTML: function(res) {
+				for (var key in windowData) {
+					if (key.indexOf("meta:") === 0) {
 						var name = key.substring(5);
 						var content = getWindowData(key);
 						
@@ -73,28 +73,28 @@ function createScope(req, res){
 		};
 	}
 	
-	function nextStyleCount(){
+	function nextStyleCount() {
 		return ++styleCount;
 	}
 	
-	function renderStyle(res){
+	function renderStyle(res) {
 		res.write(getStyles());
 	}
 	
-	function renderWindowData(res, name, value){
+	function renderWindowData(res, name, value) {
 		res.write(escapeHTML(getWindowData(name, value)));
 	}
 	
-	function style(){
+	function style() {
 		return {
 			toHTML: renderStyle,
 			toStyleContent: renderStyle
 		};
 	}
 	
-	function title(value){
+	function title(value) {
 		return {
-			toHTML: function(res){
+			toHTML: function(res) {
 				renderWindowData(res, "title", value);
 			}
 		};
@@ -116,14 +116,15 @@ function createScope(req, res){
 	};
 }
 
-function getScope(allowNullScope){
-	if(!scope && !allowNullScope){
+function getScope(allowNullScope) {
+	if (!scope && !allowNullScope) {
 		throw new Error("This function can only be called synchronously from a component");
 	}
+	
 	return scope;
 }
 
-function setScope(newScope){
+function setScope(newScope) {
 	scope = newScope;
 }
 
