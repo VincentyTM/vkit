e282993e9734f98a3a6f66f1f176344dff0f7727
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-02-25 20:57:26 +0100

    Update code style in CLI debug script

--- vkit-cli/buildDevDebugScriptElement.js
+++ vkit-cli/buildDevDebugScriptElement.js
@@ -1,54 +1,54 @@
 module.exports = () => `<script type="text/javascript" language="javascript">
 "use strict";
-window.onerror = function(message, source, lineno, colno, error){
-	function elem(tagName, children, style){
+window.onerror = function(message, source, lineno, colno, error) {
+	function elem(tagName, children, style) {
 		var el = document.createElement(tagName);
 		var n = children.length;
-		for(var i=0; i<n; ++i){
+		for (var i = 0; i < n; ++i) {
 			var child = children[i];
-			if( child ){
+			if (child) {
 				el.appendChild(child.nodeType ? child : document.createTextNode(child));
 			}
 		}
-		if( style ){
-			for(var k in style){
+		if (style) {
+			for (var k in style) {
 				el.style[k] = style[k];
 			}
 		}
 		return el;
 	}
-	function getDigitCount(number){
+	function getDigitCount(number) {
 		var count = 1;
-		while( number >= 10 ){
+		while (number >= 10) {
 			number = number / 10 | 0;
 			++count;
 		}
 		return count;
 	}
-	function padLeft(number, length, padding){
+	function padLeft(number, length, padding) {
 		number = String(number);
-		while( number.length < length ){
+		while (number.length < length) {
 			number = padding + number;
 		}
 		return number;
 	}
-	function filterRegExp(array, regexp){
+	function filterRegExp(array, regexp) {
 		var newArray = [];
 		var n = array.length;
-		for(var i=0; i<n; ++i){
-			if( regexp.test(array[i]) ){
+		for (var i = 0; i < n; ++i) {
+			if (regexp.test(array[i])) {
 				newArray.push(array[i]);
 			}
 		}
 		return newArray;
 	}
-	function FormattedCode(functionName, source, lineno, colno, lineColor, blockColor){
-		function getCodeElem(code){
+	function FormattedCode(functionName, source, lineno, colno, lineColor, blockColor) {
+		function getCodeElem(code) {
 			var lines = code.split("\\r").join("").split("\\t").join("    ").split("\\n").slice(Math.max(0, lineno - 4), lineno + 3);
 			var n = lines.length;
 			var lineOfError = lineno < 4 ? lineno - 1 : 3;
 			var maxDigits = getDigitCount(lineno - lineOfError + n - 1);
-			for(var i=0; i<n; ++i){
+			for (var i = 0; i < n; ++i) {
 				var lineText = (i === lineOfError ? " > " : "   ") + padLeft(lineno - lineOfError + i, maxDigits, " ") + " | " + lines[i];
 				lines[i] = elem("div", [lineText], i === lineOfError ? {backgroundColor: lineColor} : null);
 			}
@@ -60,9 +60,9 @@ window.onerror = function(message, source, lineno, colno, error){
 			});
 		}
 		var xhr = new XMLHttpRequest();
-		xhr.onreadystatechange = function(){
-			if( xhr.readyState === 4 && xhr.status === 200 ){
-				if( placeholder.parentNode ){
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState === 4 && xhr.status === 200) {
+				if (placeholder.parentNode) {
 					placeholder.parentNode.replaceChild(getCodeElem(xhr.responseText), placeholder);
 				}
 			}
@@ -90,11 +90,11 @@ window.onerror = function(message, source, lineno, colno, error){
 		]);
 		return msg;
 	}
-	function StackLine(line, lineColor, blockColor){
+	function StackLine(line, lineColor, blockColor) {
 		line = line.replace("new ", "");
 		var regexp = /^(\\s*at\\s+)?([^\\s@]*)\\s*@?\\s*\\(?\\s*([^)]+)\\)?$/g;
 		var match = regexp.exec(line);
-		if(!match){
+		if (!match) {
 			return null;
 		}
 		var functionName = match[2];
@@ -104,26 +104,26 @@ window.onerror = function(message, source, lineno, colno, error){
 		var ln = parseInt(sourceWithNumbers.substring(secondLastColonPos + 1, lastColonPos));
 		var cn = parseInt(sourceWithNumbers.substring(lastColonPos + 1));
 		var src = sourceWithNumbers.substring(0, secondLastColonPos).replace(/\\[[^\\]]+\\]\\s*\\(?/g, "");
-		if( ln > 0 && cn > 0 ){
+		if (ln > 0 && cn > 0) {
 			return FormattedCode(functionName, src, ln, cn, lineColor, blockColor);
-		}else{
+		} else {
 			return null;
 		}
 	}
-	function StackHiddenContents(stack){
+	function StackHiddenContents(stack) {
 		var n = stack.length;
 		var views = new Array(n - 1);
-		for(var i=1; i<n; ++i){
+		for (var i = 1; i < n; ++i) {
 			views[i] = StackLine(stack[i], "#fcf2af", "#fffbe7");
 		}
 		return elem("div", views);
 	}
 	var stack = error && error.stack ? filterRegExp(error.stack.split("\\n"), /^(\\s*at\\s+)?([^\\s@]*)\\s*@?\\s*\\(?\\s*([^)]+)\\)?$/) : [];
-	if( stack[0] && !stack[0].startsWith(" ") && stack[0].indexOf("@") === -1 ){
+	if (stack[0] && !stack[0].startsWith(" ") && stack[0].indexOf("@") === -1) {
 		stack.shift();
 	}
 	var closeButton = elem("button", ["x"], {
-		float: "right",
+		"float": "right",
 		border: "0",
 		padding: "0 0.5em",
 		margin: "0",
@@ -133,8 +133,8 @@ window.onerror = function(message, source, lineno, colno, error){
 		fontSize: "200%",
 		cursor: "pointer"
 	});
-	closeButton.onclick = function(){
-		if( container.parentNode ){
+	closeButton.onclick = function() {
+		if (container.parentNode) {
 			container.parentNode.removeChild(container);
 		}
 	};
@@ -160,7 +160,7 @@ window.onerror = function(message, source, lineno, colno, error){
 	});
 	document.body.appendChild(container);
 };
-window.onunhandledrejection = function(e){
+window.onunhandledrejection = function(e) {
 	var error = e.reason;
 	window.onerror(
 		error.message,
