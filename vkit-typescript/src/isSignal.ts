dd9f9b03ec3f23dbfb7c6868b14c583a47a09588
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-02-25 12:39:05 +0100

    Add type guard to isSignal

--- vkit-typescript/src/isSignal.ts
+++ vkit-typescript/src/isSignal.ts
@@ -1,3 +1,5 @@
-export default function isSignal(value: any): boolean {
+import {Signal} from "./signal";
+
+export default function isSignal(value: any): value is Signal<unknown> {
 	return !!(value && value.isSignal === true);
 }
