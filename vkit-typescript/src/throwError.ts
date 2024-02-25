3239ff9597f77275c24d4d507f1e156e2ea06b7f
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-02-25 22:46:33 +0100

    Add throwError type check

--- vkit-typescript/src/throwError.ts
+++ vkit-typescript/src/throwError.ts
@@ -2,6 +2,10 @@ import {Component} from "./component";
 
 export default function throwError(error: any, component: Component | null): void {
 	while (component) {
+		if (component.parent && error && typeof error.stack === "string") {
+			error.stack += "\n" + component.stack;
+		}
+		
 		if (component.emitError) {
 			try {
 				component.emitError(error);
