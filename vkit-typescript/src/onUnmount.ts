b5e7c463482e0af0f29691b25a1be2b07d630e14
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-02-25 14:02:32 +0100

    Remove no-callback version of TypeScript onUnmount

--- vkit-typescript/src/onUnmount.ts
+++ vkit-typescript/src/onUnmount.ts
@@ -8,14 +8,6 @@ export default function onUnmount(
 	callback: () => void,
 	component?: Component | null
 ) : (callback: () => void) => void {
-	if (!callback) {
-		component = getComponent();
-		
-		return function(callback: () => void) {
-			return onUnmount(callback, component);
-		};
-	}
-	
 	if (!component) {
 		component = getComponent();
 	}
