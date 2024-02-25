8fdc985d47f735d3327dd4b34cf42bc29347586b
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-02-25 22:48:21 +0100

    Make `is` independent from `computed`

--- vkit/is.js
+++ vkit/is.js
@@ -1,11 +1,26 @@
-(function($) {
+(function($, undefined) {
 
-var computed = $.computed;
+var effect = $.effect;
+var getComponent = $.getComponent;
 
 function is(condition) {
-	return computed(function() {
-		return !!condition();
-	})();
+	if (typeof condition !== "function") {
+		throw new TypeError("condition is not a function");
+	}
+	
+	var parent = getComponent();
+	var value;
+	
+	effect(function() {
+		var oldValue = value;
+		value = !!condition();
+		
+		if (oldValue !== value && oldValue !== undefined) {
+			parent.render();
+		}
+	});
+	
+	return value;
 }
 
 $.is = is;
