54e5228fca65292e1e0ec4117ee8a28cb6407196
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-03-05 22:06:09 +0100

    Update frameContent
    It only returns an array if necessary.

--- vkit/frameContent.js
+++ vkit/frameContent.js
@@ -1,27 +1,38 @@
-(function($){
+(function($) {
 
 var onEvent = $.onEvent;
 var renderDetached = $.renderDetached;
 
-function createFrameContent(component){
+function frameContent(getView) {
+	var props = {
+		onload: function() {
+			var win = this.contentWindow;
+			
+			renderDetached(function(unmount) {
+				onEvent(win, "unload", unmount);
+				
+				return (
+					typeof getView.get === "function" ? getView.get() : getView
+				)();
+			}, win.document.body);
+		}
+	};
+	
+	if (typeof getView.subscribe !== "function") {
+		return props;
+	}
+	
 	return [
-		{
-			onload: function(){
-				var win = this.contentWindow;
-				renderDetached(function(unmount){
-					onEvent(win, "unload", unmount);
-					return (typeof component.get === "function" ? component.get() : component)();
-				}, win.document.body);
-			}
-		},
-		typeof component.subscribe === "function" ? function(iframe){
-			component.subscribe(function(){
+		props,
+		
+		function(iframe) {
+			getView.subscribe(function() {
 				iframe.src = "";
 			});
-		} : null
+		}
 	];
 }
 
-$.frameContent = createFrameContent;
+$.frameContent = frameContent;
 
 })($);
