3aee1fdde71f123f9b81cef0415b1f79580d7f86
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-03-23 13:46:55 +0100

    Add TypeScript isWritableSignal method description

--- vkit-typescript/src/isWritableSignal.ts
+++ vkit-typescript/src/isWritableSignal.ts
@@ -1,5 +1,10 @@
 import type {WritableSignal} from "./signal.js";
 
+/**
+ * Takes a value and returns whether it is a writable signal.
+ * @param value The value to be checked.
+ * @returns A boolean which is true if the input value is a writable signal, false otherwise.
+ */
 export default function isWritableSignal(value: any): value is WritableSignal<unknown> {
 	return !!(value && value.isSignal === true && value.set);
 }
