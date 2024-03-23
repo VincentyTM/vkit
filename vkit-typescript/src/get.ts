99d30f4b0e77be2039d9d736708a3eac15af6ceb
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-03-23 14:06:24 +0100

    Add TypeScript get method description

--- vkit-typescript/src/get.ts
+++ vkit-typescript/src/get.ts
@@ -1,6 +1,17 @@
 import isSignal from "./isSignal.js";
 import type {Signal} from "./signal.js";
 
+/**
+ * Returns the current value of a signal or the input value itself if it is not a signal.
+ * It is a shortcut for `isSignal(value) ? value.get() : value`.
+ * @example
+ * get("Something") === "Something"
+ * get(signal("Something")) === "Something"
+ * 
+ * @param signalOrValue If it is a signal, the method returns its current value.
+ * Otherwise, the method returns it without any change.
+ * @returns The content of the input signal or the raw input value.
+ */
 export default function get<ValueType>(signalOrValue: ValueType | Signal<ValueType>): ValueType {
 	return isSignal(signalOrValue) ? signalOrValue.get() : signalOrValue;
 }
