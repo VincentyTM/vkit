5744eceb0a4fe96d6b54ed64b03a0fda4096353c
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-03-23 13:54:54 +0100

    Update shadow type
    * The parameters are context-aware now.
    * The return type is more general.

--- vkit-typescript/src/shadow.ts
+++ vkit-typescript/src/shadow.ts
@@ -2,13 +2,13 @@ import bind from "./bind.js";
 import append from "./append.js";
 import type {View} from "./view.js";
 
-export default function shadow(...args: View[]): (element: HTMLElement) => void;
+export default function shadow(...contents: View<ShadowRoot>[]): View<HTMLElement>;
 
-export default function shadow(): (element: HTMLElement) => void {
-	var args: View = arguments;
+export default function shadow(): View<HTMLElement> {
+	var contents: View<ShadowRoot> = arguments;
 	
 	return function(element: HTMLElement): void {
 		var shadowRoot = element.shadowRoot || element.attachShadow({mode: "open"});
-		append(shadowRoot as Node, args, shadowRoot, bind);
+		append(shadowRoot, contents, shadowRoot, bind);
 	};
 }
