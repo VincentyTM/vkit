8a78bddff7d24fe460b8820e6b9475b9507d512c
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-03-23 14:16:31 +0100

    Add TypeScript text method description

--- vkit-typescript/src/text.ts
+++ vkit-typescript/src/text.ts
@@ -1,5 +1,17 @@
 import createComponent from "./createComponent.js";
 
+/**
+ * Creates and returns a dynamic text node.
+ * Its text changes when at least one of its inputs change.
+ * Inputs can be added by calling signals in the method's `getText` callback function.
+ * @example
+ * const name = signal("world");
+ * const myTextNode = text(() => `Hello ${name()}`);
+ * 
+ * @param getText A function that returns the current text.
+ * It may contain signal calls.
+ * @returns A DOM text node that has the current text as its value.
+ */
 export default function text(getText: () => string | number): Text {
 	var oldText = "";
 	var node = document.createTextNode(oldText);
