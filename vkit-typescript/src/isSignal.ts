0f3d4b895793c2d375328ed5e3e76efe646f0149
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-03-23 13:46:38 +0100

    Add TypeScript isSignal method description

--- vkit-typescript/src/isSignal.ts
+++ vkit-typescript/src/isSignal.ts
@@ -1,5 +1,10 @@
 import type {Signal} from "./signal.js";
 
+/**
+ * Takes a value and returns whether it is a signal.
+ * @param value The value to be checked.
+ * @returns A boolean which is true if the input value is a signal, false otherwise.
+ */
 export default function isSignal(value: any): value is Signal<unknown> {
 	return !!(value && value.isSignal === true);
 }
