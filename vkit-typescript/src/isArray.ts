3268007d35e9baad1e785214b6bca339cbb419df
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-03-23 13:46:15 +0100

    Add TypeScript isArray method description

--- vkit-typescript/src/isArray.ts
+++ vkit-typescript/src/isArray.ts
@@ -1,5 +1,10 @@
 var toString = Object.prototype.toString;
 
+/**
+ * Takes a value and returns whether it is an array.
+ * @param value The value to be checked.
+ * @returns A boolean which is true if the input value is an array, false otherwise.
+ */
 function isArray(value: any): value is unknown[] {
 	return toString.call(value) === "[object Array]";
 }
