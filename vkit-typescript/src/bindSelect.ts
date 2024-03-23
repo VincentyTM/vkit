01bff1e0ad729965f0f7f6b195b94095cb3ae2e1
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-03-23 13:43:22 +0100

    Add TypeScript bindSelect method description

--- vkit-typescript/src/bindSelect.ts
+++ vkit-typescript/src/bindSelect.ts
@@ -2,6 +2,24 @@ import tick from "./tick.js";
 import type {WritableSignal} from "./signal.js";
 import type {View} from "./view.js";
 
+/**
+ * Sets up a two-way data binding between a writable signal and a <select> HTML element.
+ * When the value of the signal changes, so does the select's value and vice versa.
+ * @example
+ * function SelectBinding() {
+ * 	const value = signal("banana");
+ * 	
+ * 	return Select(
+ * 		bindSelect(value),
+ * 		Option({value: "apple", label: "Apple"}),
+ * 		Option({value: "banana", label: "Banana"}),
+ * 		Option({value: "orange", label: "Orange"})
+ * 	);
+ * }
+ * 
+ * @param signal A signal that can be read from and written to.
+ * @returns A directive that can be applied on a <select> element.
+ */
 export default function bindSelect(signal: WritableSignal<string>): View<HTMLSelectElement> {
 	return [
 		function(el) {
