fe77ad5375a204f68beba0a5d2656232b58b4fdf
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-03-29 00:25:58 +0100

    Create TypeScript renderDetached module

---
+++ vkit-typescript/src/renderDetached.ts
@@ -0,0 +1,52 @@
+import append from "./append.js";
+import bind from "./bind.js";
+import createComponent from "./createComponent.js";
+import createInjector from "./createInjector.js";
+import createProvider from "./provider.js";
+import emitUnmount from "./emitUnmount.js";
+import {getValueFromClass} from "./root.js";
+import inject from "./inject.js";
+import update from "./update.js";
+import type {View} from "./view.js";
+import {WindowService} from "./getWindow.js";
+
+export default function renderDetached<C extends Node>(
+    getView: (unmount: () => void) => View<C>,
+    container: C
+): () => void {
+    var injector = createInjector(null, function(token) {
+        var provider = createProvider(getValueFromClass, token, component);
+        injector.container.set(token, provider);
+        return provider.getInstance();
+    });
+    
+    var component = createComponent(function(): void {
+        var win: (Window & typeof globalThis) | null = null;
+        
+        if (container) {
+            var doc = container.ownerDocument;
+
+            if (doc) {
+                win = doc.defaultView || (doc as any).parentWindow;
+            }
+        }
+        
+        if (win) {
+            inject(WindowService).window = win;
+        }
+        
+        var view = getView(unmount);
+        
+        if (container) {
+            append(container, view, container, bind);
+        }
+    }, null, injector);
+
+    function unmount(): void {
+        emitUnmount(component);
+    }
+    
+    component.render();
+    update();
+    return unmount;
+}
