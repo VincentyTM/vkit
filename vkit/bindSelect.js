b26bdd79244de95528f3fa5c8c3803cb98e1ad65
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-03-29 00:34:36 +0100

    Change error message in bindSelect

--- vkit/bindSelect.js
+++ vkit/bindSelect.js
@@ -5,7 +5,7 @@ var tick = $.tick;
 
 function bindSelect(signal) {
 	if (!isWritableSignal(signal)) {
-		throw new TypeError("A <select> can only be bound to a writable signal");
+		throw new TypeError("Two-way data binding needs a writable signal");
 	}
 	
 	return [
