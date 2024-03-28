2f649a75e7a10602255783da3a8d825c5810906d
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-03-29 00:35:59 +0100

    Add TypeScript is method description

--- vkit-typescript/src/is.ts
+++ vkit-typescript/src/is.ts
@@ -1,6 +1,26 @@
 import effect from "./effect.js";
 import {getComponent} from "./contextGuard.js";
 
+/**
+ * Evaluates a function returning a boolean and rerenders the current view/views/effect block when that boolean value changes.
+ * 
+ * See the following example. If `is` was not used there, the view block would be rerendered every time `count` changes.
+ * @example
+ * function Counter() {
+ * 	const count = signal(5);
+ * 	
+ * 	return view(() => {
+ * 		if (is(() => count() > 3)) {
+ * 			return "Count is more than 3!";
+ * 		} else {
+ * 			return "Count is less than or equal to 3!";
+ * 		}
+ * 	});
+ * }
+ * 
+ * @param condition The function that returns a boolean. It can have reactive inputs in it.
+ * @returns The return value of the `condition` function.
+ */
 export default function is(condition: () => boolean): boolean {
 	var parent = getComponent();
 	var value: boolean | undefined;
