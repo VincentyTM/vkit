2bd0b3004211ff30ff1ca9ce9a52ff5fca31e965
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-03-29 00:19:05 +0100

    Narrow down types in prop

--- vkit-typescript/src/prop.ts
+++ vkit-typescript/src/prop.ts
@@ -1,12 +1,16 @@
 import effect from "./effect.js";
 
-export default function prop<ElementType, ValueType>(
-	name: string,
-	getValue: () => ValueType
-) {
-	return function(element: ElementType) {
+type ElementType<K extends string, V> = {
+	[N in K]: V;
+};
+
+export default function prop<K extends string, V>(
+	name: K,
+	getValue: () => V
+): (element: ElementType<K, V>) => void {
+	return function(element: ElementType<K, V>): void {
 		effect(function() {
-			(element as any)[name] = getValue();
+			element[name] = getValue();
 		});
 	};
 }
