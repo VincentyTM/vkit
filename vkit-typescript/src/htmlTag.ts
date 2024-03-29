086e8177e51ff20e3c201e8d95c56e0c0fc5b023
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-03-29 18:33:32 +0100

    Update htmlTag type
    Use View instead of HTMLElement.

--- vkit-typescript/src/htmlTag.ts
+++ vkit-typescript/src/htmlTag.ts
@@ -33,8 +33,8 @@ import type {View} from "./view.js";
  */
 export default function htmlTag<K extends keyof HTMLElementTagNameMap>(tagName: K): (
 	...contents: View<HTMLElementTagNameMap[K]>[]
-) => HTMLElementTagNameMap[K] {
-	return function(): HTMLElementTagNameMap[K] {
+) => View<Element> {
+	return function(): View<Element> {
 		var el = document.createElement(tagName);
 		append<View<typeof el>, typeof el>(el, arguments, el, bind);
 		return el;
