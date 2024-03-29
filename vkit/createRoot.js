90d329152cbecbb23e01e2c18f884358536af9f2
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-03-29 18:23:39 +0100

    Create createRoot component

---
+++ vkit/createRoot.js
@@ -0,0 +1,26 @@
+(function($) {
+
+var empty = $.empty;
+var noop = $.noop;
+var renderDetached = $.renderDetached;
+
+function createRoot(container) {
+    var unmount = noop;
+
+    return {
+        render: function(rootComponent) {
+            unmount();
+            empty(container);
+            unmount = renderDetached(rootComponent, container);
+        },
+
+        unmount: function() {
+            unmount();
+            unmount = noop;
+        }
+    };
+}
+
+$.createRoot = createRoot;
+
+})($);
