324ee56f19ea13ffdde8ba426fc5e9a0a5309331
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-03-29 19:43:32 +0100

    Skip unnecessary onUnmount in on

--- vkit/on.js
+++ vkit/on.js
@@ -1,11 +1,24 @@
 (function($) {
 
+var getComponent = $.getComponent;
 var onEvent = $.onEvent;
 var onUnmount = $.onUnmount;
 
 function on(type, action) {
+	var outer = getComponent(true);
+	
 	return function(el) {
-		onUnmount(onEvent(el, type, action));
+		var inner = getComponent(true);
+		
+		if (!inner && outer) {
+			getComponent();
+		}
+		
+		var unsubscribe = onEvent(el, type, action);
+		
+		if (outer !== inner) {
+			onUnmount(unsubscribe);
+		}
 	};
 }
 
