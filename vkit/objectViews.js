ff5127b7c154efc7b61d83e8ac22a824a8f08251
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-04-12 20:58:24 +0200

    Update code style in objectViews

--- vkit/objectViews.js
+++ vkit/objectViews.js
@@ -1,26 +1,26 @@
-(function($){
+(function($) {
 
-var getKeys = Object.keys || function(object){
+var getKeys = Object.keys || function(object) {
 	var keys = [];
 	
-	for(var key in object){
+	for (var key in object) {
 		keys.push(key);
 	}
 	
 	return keys;
 };
 
-function objectViews(recordsState, getView, factory){
-	return recordsState.map(getKeys).views(function(key){
-		var itemState = typeof recordsState.select === "function"
-			? recordsState.select(key, factory)
-			: recordsState.map(
-				function(records){ return records[key]; }
-			);
+function objectViews(records, getItemView, factory) {
+	return records.map(getKeys).views(function(key) {
+		var item = typeof records.select === "function"
+			? records.select(key, factory)
+			: records.map(function(records) {
+				return records[key];
+			});
 		
-		itemState.key = key;
+		item.key = key;
 		
-		return getView(itemState);
+		return getItemView(item);
 	});
 }
 
