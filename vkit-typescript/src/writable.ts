e8e7799d2ba1f0433138ac17fd9448be0a730973
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-04-12 21:08:20 +0200

    Create TypeScript writable module

---
+++ vkit-typescript/src/writable.ts
@@ -0,0 +1,13 @@
+import type {ComputedSignal} from "./computed.js";
+import {add, toggle, toString, update, type Signal, type WritableSignal} from "./signal.js";
+
+export default function writable<T, U extends T>(input: ComputedSignal<T>, setValue: (value: U) => void): WritableSignal<T> {
+    var output = input as Signal<T> as WritableSignal<T>;
+    output.add = add;
+    output.set = setValue;
+    output.setEagerly = setValue;
+    output.toggle = toggle;
+    output.toString = toString;
+    output.update = update;
+    return output;
+}
