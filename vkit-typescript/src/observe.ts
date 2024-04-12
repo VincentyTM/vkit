81d1fb20af18dcc35f03efd72e9a88ecea8ac3c0
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-04-12 20:51:53 +0200

    Update property type in observe

--- vkit-typescript/src/observe.ts
+++ vkit-typescript/src/observe.ts
@@ -3,10 +3,7 @@ import observable, {type Observable} from "./observable.js";
 var defineProperty = Object.defineProperty;
 var getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;
 
-export default function observe<ObjectType>(
-	object: ObjectType,
-	property: keyof ObjectType
-): Observable<ObjectType[keyof ObjectType]> | null {
+export default function observe<T, K extends keyof T>(object: T, property: K): Observable<T[K]> | null {
 	var desc = getOwnPropertyDescriptor(object, property);
 	
 	if (!desc) {
@@ -14,20 +11,20 @@ export default function observe<ObjectType>(
 	}
 	
 	if (desc.get && (desc.get as any).emitChange) {
-		return (desc.get as any).emitChange as Observable<ObjectType[keyof ObjectType]>;
+		return (desc.get as any).emitChange as Observable<T[K]>;
 	}
 	
 	var value = object[property];
 	
-	function get(): ObjectType[keyof ObjectType] {
+	function get(): T[K] {
 		return value;
 	}
 	
-	var emitChange = get.emitChange = observable<ObjectType[keyof ObjectType]>();
+	var emitChange = get.emitChange = observable<T[K]>();
 	
 	defineProperty(object, property, {
 		get: get,
-		set: function(v: ObjectType[keyof ObjectType]): void {
+		set: function(v: T[K]): void {
 			if (value !== v) {
 				value = v;
 				emitChange(v);
