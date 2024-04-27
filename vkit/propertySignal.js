19d3eff5af4cf1bd796435df0e5456a848fd0275
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-04-27 14:05:49 +0200

    Add type checks in propertySignal

--- vkit/propertySignal.js
+++ vkit/propertySignal.js
@@ -4,15 +4,27 @@ var objectAssign = $.objectAssign;
 var writable = $.writable;
 
 function propertySignal(parent, key, defaultValue) {
+	if (!isWritableSignal(parent)) {
+		throw new TypeError("Parent of a property signal must be a writable signal");
+	}
+	
     function selectValue(state) {
-		if (state === undefined || state === null) {
-			return state;
+		if (state === undefined) {
+			throw new TypeError("Object must not be undefined");
+		}
+		
+		if (state === null) {
+			throw new TypeError("Object must not be null");
 		}
 		
         var current = state[key];
 		
 		if (current !== undefined) {
 			return current;
+		}
+		
+		if (defaultValue === undefined) {
+			throw new TypeError("Property '" + key + "' does not exist and there is no default value provided");
 		}
 		
         return defaultValue;
