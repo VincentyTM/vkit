e745e78137afee9e315c9a052a236582c88bd5a4
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-04-27 15:10:26 +0200

    Add propertySignal method description

--- vkit-typescript/src/propertySignal.ts
+++ vkit-typescript/src/propertySignal.ts
@@ -4,6 +4,31 @@ import objectAssign from "./objectAssign.js";
 import type {Signal, WritableSignal} from "./signal.js";
 import writable from "./writable.js";
 
+/**
+ * Creates and returns a writable signal that reflects and updates a property of the current value of another writable signal.
+ * In contrast to `objectProperty`, it maintains an immutable data structure and replaces the parent object instead of modifying it.
+ * Writing a property signal is delayed until the next update, unlike a regular writable signal.
+ * @example
+ * interface Person {
+ * 	firstName: string;
+ * 	lastName: string;
+ * 	age?: number;
+ * }
+ * 
+ * const person = signal<Person>({
+ * 	firstName: "John",
+ * 	lastName: "Smith"
+ * });
+ * 
+ * const firstName = propertySignal(person, "firstName");
+ * const lastName = propertySignal(person, "lastName");
+ * const age = propertySignal(person, "age", 42);
+ * 
+ * @param parent The signal that contains the object.
+ * @param key The key of the property of the object. It may be a signal if it can change dynamically.
+ * @param defaultValue An optional default value to replace undefined.
+ * @returns A writable signal that contains the property's current value.
+ */
 export default function propertySignal<P, K extends keyof P>(
 	parent: WritableSignal<P>,
 	key: K | Signal<K>,
