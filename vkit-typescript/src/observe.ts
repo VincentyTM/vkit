77a66e4dcf34176326786a1a5741b956515b1aa5
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-04-27 15:11:23 +0200

    Add observe method description

--- vkit-typescript/src/observe.ts
+++ vkit-typescript/src/observe.ts
@@ -3,6 +3,24 @@ import observable, {type Observable} from "./observable.js";
 var defineProperty = Object.defineProperty;
 var getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;
 
+/**
+ * Observes a property of an object and returns an observable that fires when the property changes.
+ * An object property can have multiple subscribers.
+ * @example
+ * const object = {count: 0};
+ * const countChange = observe(object, "count");
+ * 
+ * if (countChange) {
+ * 	countChange.subscribe((count) => console.log("Count is " + count));
+ * }
+ * 
+ * object.count += 10;
+ * object.count = 42;
+ * 
+ * @param object The object to be observed.
+ * @param property The key of a property of the object.
+ * @returns An observable that fires with the new value when the property changes or `null` if the subscription is unsuccessful.
+ */
 export default function observe<T, K extends keyof T>(object: T, property: K): Observable<T[K]> | null {
 	var desc = getOwnPropertyDescriptor(object, property);
 	
