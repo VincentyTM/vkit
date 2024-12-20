6a2ce2e8cdbaf90222bf136a7069422d12bcee13
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-12-20 21:53:54 +0100

    Change export syntax in TypeScript root

--- vkit-typescript/src/root.ts
+++ vkit-typescript/src/root.ts
@@ -11,12 +11,10 @@ export function getValueFromClass(config: Config): InstanceType<ConfigClass> {
 	return new (config as ConfigClass)();
 }
 
-var rootInjector = createInjector(null, function(token): InstanceType<ConfigClass> {
+export var rootInjector = createInjector(null, function(token): InstanceType<ConfigClass> {
 	var provider = createProvider(getValueFromClass, token, rootComponent);
 	rootInjector.container.set(token, provider);
 	return provider.getInstance();
 });
 
-var rootComponent = createComponent(mount, null, rootInjector);
-
-export {rootComponent, rootInjector};
+export var rootComponent = createComponent(mount, null, rootInjector);
