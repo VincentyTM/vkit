5ceb6393e80ca24746dcae7c5b9097c35a6eb2e6
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2024-12-22 19:30:58 +0100

    Replace http module with TypeScript version

--- vkit/http.js
+++ vkit/http.js
@@ -1,223 +1,131 @@
 (function($, undefined) {
 
-var effect = $.effect;
-var observable = $.observable;
+var computed = $.computed;
+var isSignal = $.isSignal;
 var onUnmount = $.onUnmount;
 var readOnly = $.readOnly;
 var signal = $.signal;
 var update = $.update;
 
-function createRequest(data) {
-	if (!data) {
-		data = {};
-	}
-	
-	var request = {
-		url: null,
-		method: "GET",
-		async: true,
-		headers: {},
-		responseType: "text",
-		mimeType: undefined,
-		withCredentials: undefined,
-		user: null,
-		password: null,
-		body: null,
-		abortable: true
-	};
-	
-	if (typeof data === "string") {
-		request.url = data;
-	} else if (data && typeof data === "object") {
-		for (var key in data) {
-			if (key in request) {
-				request[key] = data[key];
-			}
-		}
-	}
-	
-	return request;
-}
+var UNSENT = {
+    ok: false,
+    progress: null,
+    unsent: true,
+    uploadProgress: null
+};
 
-function sendRequest(request, pendingResponse, responseState, complete) {
-	if (request.url === null || request.url === undefined) {
-		return null;
-	}
+var INIITIAL_PROGRESS = {
+    loaded: 0,
+    total: 0,
+    lengthComputable: false
+};
+
+function http(request) {
+    var response = signal(UNSENT);
 	
-	var xhr = new XMLHttpRequest();
-	
-	xhr.onprogress = function(e) {
-		pendingResponse.progress.set(e);
-		update();
-	};
-	
-	if (xhr.upload) {
-		xhr.upload.onprogress = function(e) {
-			pendingResponse.uploadProgress.set(e);
-			update();
-		};
-	}
-	
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState === 4 || xhr.readyState === 0) {
-			var response = createResponse(xhr);
-			responseState.set(response);
-			complete(response);
-		}
+    function setRequest(req) {
+        if (req === null) {
+            response.set(UNSENT);
+            return;
+        }
 		
-		update();
-	};
-	
-	xhr.open(
-		request.method,
-		request.url,
-		request.async,
-		request.user,
-		request.password
-	);
-	
-	if (request.mimeType !== undefined && xhr.overrideMimeType) {
-		xhr.overrideMimeType(request.mimeType);
-	}
-	
-	xhr.responseType = request.responseType;
-	
-	if (request.withCredentials !== undefined) {
-		xhr.withCredentials = request.withCredentials;
-	}
-	
-	var headers = request.headers;
-	
-	for (var name in headers) {
-		xhr.setRequestHeader(name, headers[name]);
-	}
-	
-	xhr.send(request.body);
-	
-	return xhr;
-}
-
-function createProgressEvent() {
-	return {
-		lengthComputable: false,
-		loaded: 0,
-		total: 0
-	};
-}
-
-function createResponse(xhr) {
-	function getHeader(name) {
-		return xhr.getResponseHeader(name);
-	}
-	
-	function getHeaders() {
-		return xhr.getAllResponseHeaders();
-	}
-	
-	var status = xhr.status;
-	
-	return {
-		ok: status >= 200 && status <= 299,
-		status: status,
-		header: getHeader,
-		headers: getHeaders,
-		body: xhr.response !== undefined ? xhr.response : xhr.responseText
-	};
-}
-
-function createPendingResponse(abort) {
-	return {
-		abort: abort,
-		progress: signal(createProgressEvent()),
-		uploadProgress: signal(createProgressEvent())
-	};
-}
-
-function createResponseState(requestData, options, onAbort) {
-	function abort() {
-		if (xhr && isAbortable) {
-			xhr.onreadystatechange = null;
-			xhr.onprogress = null;
+        var progress = signal(INIITIAL_PROGRESS);
+        var uploadProgress = signal(INIITIAL_PROGRESS);
+		
+        response.set({
+            ok: false,
+            progress: readOnly(progress),
+            unsent: false,
+            uploadProgress: readOnly(uploadProgress)
+        });
+		
+        var xhr = new XMLHttpRequest();
+		
+        xhr.onprogress = function(event) {
+            progress.set(event);
+            update();
+        };
+		
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 || xhr.readyState === 0) {
+                var status = xhr.status;
+				
+                response.set({
+                    body: xhr.response !== undefined ? xhr.response : xhr.responseText,
+                    ok: status >= 200 && status <= 299,
+                    progress: null,
+                    status: status,
+                    unsent: false,
+                    uploadProgress: null,
+					
+                    getAllResponseHeaders: function() {
+                        return xhr.getAllResponseHeaders();
+                    },
+					
+                    getResponseHeader: function(name) {
+                        return xhr.getResponseHeader(name);
+                    }
+                });
+				
+                update();
+            }
+        };
+		
+        if (xhr.upload) {
+            xhr.upload.onprogress = function(event) {
+                uploadProgress.set(event);
+                update();
+            };
+        }
+		
+        if (typeof req === "string") {
+            xhr.open("GET", req, true);
+            xhr.send();
+        } else {
+            xhr.open((req.method || "GET").toUpperCase(), req.url || "", req.async !== false, req.user, req.password);
 			
-			if (xhr.upload) {
-				xhr.upload.onprogress = null;
-			}
+            if (req.mimeType !== undefined && xhr.overrideMimeType) {
+                xhr.overrideMimeType(req.mimeType);
+            }
 			
-			xhr.abort();
-		}
+            xhr.responseType = req.responseType || "";
+			
+            if (req.withCredentials !== undefined) {
+                xhr.withCredentials = req.withCredentials;
+            }
+			
+            var headers = req.headers;
+			
+            if (headers) {
+                for (var name in headers) {
+                    xhr.setRequestHeader(name, headers[name]);
+                }
+            }
+			
+            xhr.send(req.body);
+        }
 		
-		xhr = null;
-		unsubscribe();
-		unsubscribe.clear();
-	}
+        onUnmount(function() {
+            xhr.onprogress = null;
+            xhr.onreadystatechange = null;
+			
+            if (xhr.upload) {
+                xhr.upload.onprogress = null;
+            }
+			
+            xhr.abort();
+        });
+    }
 	
-	function setRequest(req) {
-		abort();
-		
-		var request = createRequest(req);
-		
-		if (options && typeof options === "object") {
-			for (var key in options) {
-				if (key in request) {
-					request[key] = options[key];
-				}
-			}
-		}
-		
-		var method = request.method.toUpperCase();
-		isAbortable = request.abortable;
-		
-		var pendingResponse = createPendingResponse(abort);
-		xhr = sendRequest(request, pendingResponse, responseState, complete);
-		responseState.set(xhr ? pendingResponse : {unsent: true});
-		
-		if (xhr) {
-			unsubscribe.subscribe(onAbort.subscribe(abort));
-		}
-	}
+    if (typeof request === "function") {
+        (isSignal(request) ? request : computed(request)).effect(setRequest);
+    } else {
+        setRequest(request);
+    }
 	
-	function complete(response) {
-		onResponse(response);
-		onResponse.clear();
-		abort();
-	}
-	
-	var onResponse = observable();
-	var unsubscribe = observable();
-	var xhr = null;
-	var isAbortable = true;
-	var responseState = signal();
-	
-	if (typeof requestData === "function") {
-		if (typeof requestData.effect === "function") {
-			requestData.effect(setRequest);
-		} else {
-			effect(function() {
-				setRequest(requestData());
-			});
-		}
-	} else {
-		setRequest(requestData);
-	}
-	
-	var result = readOnly(responseState);
-	result.then = onResponse.subscribe;
-	return result;
+    return readOnly(response);
 }
 
-function createHttpHandle(request, options) {
-	var abort = observable();
-	var hasAborter = options && typeof options.aborter === "function";
-	
-	if (hasAborter) {
-		options.aborter(abort);
-	} else {
-		onUnmount(abort);
-	}
-	
-	return createResponseState(request, options, abort);
-}
-
-$.http = createHttpHandle;
+$.http = http;
 
 })($);
