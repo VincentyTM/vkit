5b278ff247710a7508585a024ffd4f664d2cbdb8
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2025-03-29 18:35:07 +0100

    Create getGlobalContext module

---
+++ vkit-typescript/src/getGlobalContext.ts
@@ -0,0 +1,8 @@
+import { WindowService } from "./getWindow.js";
+import { inject } from "./inject.js";
+
+var global: typeof globalThis = typeof globalThis === "object" ? globalThis : self;
+
+export function getGlobalContext(): typeof globalThis {
+    return inject(WindowService).window || global;
+}
