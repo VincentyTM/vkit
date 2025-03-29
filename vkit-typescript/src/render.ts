3a13e00bd975526be4380ca2e62a310d2340dc72
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2025-03-29 18:26:47 +0100

    Add RenderRoot return value to render

--- vkit-typescript/src/render.ts
+++ vkit-typescript/src/render.ts
@@ -2,10 +2,15 @@ import { append } from "./append.js";
 import { bind } from "./bind.js";
 import { createEffect } from "./createEffect.js";
 import { createInjector } from "./createInjector.js";
+import { destroyEffect } from "./destroyEffect.js";
 import { WindowService } from "./getWindow.js";
 import { inject } from "./inject.js";
 import { Template } from "./Template.js";
 import { updateEffect } from "./updateEffect.js";
+
+export interface RenderRoot {
+	destroy(): void;
+}
 
 /**
  * Renders the root component of the application in the DOM.
@@ -17,15 +22,16 @@ import { updateEffect } from "./updateEffect.js";
  * }
  * 
  * // This is the entry point of the application
- * render(App, document.body);
+ * const appRoot = render(App, document.body);
+ * 
+ * // Destroy the effects after 1 second
+ * setTimeout(() => appRoot.destroy(), 1000);
  * 
  * @param getTemplate The top-level component. It must be a function.
  * @param container A container DOM node in which the application is rendered.
+ * @returns An object that can be used to destroy the rendered effects.
  */
-export function render<P extends Node>(
-	getTemplate: () => Template<P>,
-	container: P
-): void {
+export function render<P extends Node>(getTemplate: () => Template<P>, container: P): RenderRoot {
 	var doc = container.ownerDocument;
 	var win: (Window & typeof globalThis) | null = doc && doc.defaultView || (doc as any).parentWindow || null;
 
@@ -42,4 +48,10 @@ export function render<P extends Node>(
 	});
 
 	updateEffect(rootEffect);
+	
+	return {
+		destroy: function(): void {
+			destroyEffect(rootEffect);
+		}
+	};
 }
