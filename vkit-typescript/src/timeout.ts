b37baa3a0bdd29c6a4a7d772fe82cab733272aad
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2025-03-29 18:38:35 +0100

    Allow server support for timeout

--- vkit-typescript/src/timeout.ts
+++ vkit-typescript/src/timeout.ts
@@ -1,8 +1,9 @@
-import { getWindow } from "./getWindow.js";
+import { effect } from "./effect.js";
+import { getGlobalContext } from "./getGlobalContext.js";
 import { onDestroy } from "./onDestroy.js";
 
 /**
- * Sets a timeout in the current window and clears it when the current reactive context is destroyed.
+ * Sets a timeout in the current global context and clears it when the current reactive context is destroyed.
  * @example
  * timeout(() => {
  * 	console.log("Timeout is over!");
@@ -10,12 +11,12 @@ import { onDestroy } from "./onDestroy.js";
  * The @param callback function runs once after @param delay milliseconds.
  */
 export function timeout(callback: () => void, delay: number): void {
-	var win = getWindow()!;
-	var timeout = win.setTimeout(callback, delay);
-	
-	onDestroy(clear);
-	
-	function clear(): void {
-		win.clearTimeout(timeout);
-	}
+	effect(function(): void {
+		var global = getGlobalContext();
+		var timeout = global.setTimeout(callback, delay);
+		
+		onDestroy(function(): void {
+			global.clearTimeout(timeout);
+		});
+	});
 }
