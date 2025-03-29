e371d00dd6facfd7ebdd5d1be678b7a83f54bb24
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2025-03-29 18:27:19 +0100

    Remove renderDetached module

--- vkit-typescript/src/frameContent.ts
+++ vkit-typescript/src/frameContent.ts
@@ -1,9 +1,8 @@
 import { Signal } from "./computed.js";
 import { directive } from "./directive.js";
-import { get } from "./get.js";
 import { isSignal } from "./isSignal.js";
 import { onEvent } from "./onEvent.js";
-import { renderDetached } from "./renderDetached.js";
+import { render } from "./render.js";
 import { Template } from "./Template.js";
 
 export function frameContent(getView: (() => Template<HTMLBodyElement>) | Signal<() => Template<HTMLBodyElement>>): Template<HTMLIFrameElement> {
@@ -15,13 +14,16 @@ export function frameContent(getView: (() => Template<HTMLBodyElement>) | Signal
                 throw new Error("Content window is null");
             }
 			
-			renderDetached(function(unmount: () => void): Template<HTMLBodyElement> {
+			var root = render(function(): Template<HTMLBodyElement> {
                 if (!win) {
                     throw new Error("Content window is null");
                 }
 
-				onEvent(win, "unload", unmount);
-				return get(getView)();
+				onEvent(win, "unload", function(): void {
+					root.destroy();
+				});
+
+				return (isSignal(getView) ? getView.get() : getView)();
 			}, win.document.body as HTMLBodyElement);
 		}
 	};
