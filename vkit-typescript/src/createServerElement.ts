460325079409b493bab75cb92b53bd525d28ecd8
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2025-03-29 10:11:17 +0100

    Create createServerElement module

---
+++ vkit-typescript/src/createServerElement.ts
@@ -0,0 +1,134 @@
+import { toKebabCase } from "./toKebabCase.js";
+
+export type ServerNode = ServerElement | ServerRawHTMLSource | string | null;
+
+export interface ServerElement {
+	readonly attributes: Record<string, string>;
+	readonly children: ServerNode[] | null;
+	readonly styleProps: Record<string, string>;
+	readonly tagName: string;
+	readonly tagNameLower: string;
+}
+
+export interface ServerRawHTMLSource {
+	readonly text: string;
+}
+
+var selfClosingTags = {
+	area: 1,
+	base: 1,
+	br: 1,
+	col: 1,
+	embed: 1,
+	hr: 1,
+	img: 1,
+	input: 1,
+	link: 1,
+	meta: 1,
+	param: 1,
+	source: 1,
+	track: 1,
+	wbr: 1
+} as const;
+
+export function createServerElement(tagName: string): ServerElement {
+	var tagNameLower = tagName.toLowerCase();
+
+	return {
+		attributes: {},
+		children: tagNameLower in selfClosingTags ? null : [],
+		styleProps: {},
+		tagName: tagName,
+		tagNameLower: tagNameLower
+	};
+}
+
+function propToAttr(name: string): string {
+	if (name === "className") {
+		return "class";
+	}
+
+	if (name === "htmlFor") {
+		return "for";
+	}
+
+	if (name.indexOf("aria") === 0) {
+		return toKebabCase(name);
+	}
+	
+	return name.toLowerCase();
+}
+
+export function appendChild(element: ServerElement, child: ServerNode): void {
+	var children = element.children;
+
+	if (children) {
+		children.push(child);
+	}
+}
+
+function getAttribute(element: ServerElement, name: string): string | null {
+	var attributes = element.attributes;
+
+	return name in attributes ? attributes[name] : null;
+}
+
+export function getInnerText(element: ServerElement): string {
+	var children = element.children;
+
+	if (!children) {
+		return "";
+	}
+
+	var parts: string[] = [];
+	var n = children.length;
+	
+	for (var i = 0; i < n; ++i) {
+		var child = children[i];
+		
+		if (typeof child === "string") {
+			parts.push(child);
+		} else if (child) {
+			if ("tagName" in child) {
+				parts.push(getInnerText(child));
+			} else {
+				parts.push(child.text);
+			}
+		}
+	}
+	
+	return parts.join("");
+}
+
+export function getProperty(element: ServerElement, name: string): string | null {
+	return getAttribute(element, propToAttr(name));
+}
+
+export function setProperty(element: ServerElement, name: string, value: unknown): void {
+	if (typeof value === "boolean") {
+		if (value) {
+			element.attributes[propToAttr(name)] = "";
+		} else {
+			delete element.attributes[propToAttr(name)];
+		}
+		return;
+	}
+	
+	if (typeof value === "string") {
+		if (name === "value" && element.tagNameLower === "textarea") {
+			appendChild(element, value);
+		} else {
+			element.attributes[propToAttr(name)] = value;
+		}
+		return;
+	}
+	
+	if (typeof value === "number") {
+		if (name === "value" && element.tagNameLower === "textarea") {
+			appendChild(element, String(value));
+		} else {
+			element.attributes[propToAttr(name)] = String(value);
+		}
+		return;
+	}
+}
