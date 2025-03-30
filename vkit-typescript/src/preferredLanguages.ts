8c5d0e7b741e2031c9eb33c38e6cef774dad3dee
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2025-03-30 09:43:08 +0200

    Add server support for preferredLanguages

--- vkit-typescript/src/preferredLanguages.ts
+++ vkit-typescript/src/preferredLanguages.ts
@@ -1,39 +1,61 @@
 import { computed, Signal } from "./computed.js";
 import { getWindow } from "./getWindow.js";
+import { inject } from "./inject.js";
 import { onDestroy } from "./onDestroy.js";
 import { onEvent } from "./onEvent.js";
+import { RenderConfigService } from "./RenderConfigService.js";
 
 function getEmptyArray(): [] {
-    return [];
+	return [];
 }
 
 function getPreferredLanguages(nav: Navigator): readonly string[] {
-    return nav.languages || [
-        nav.language ||
-        (nav as any).browserLanguage ||
-        (nav as any).systemLanguage ||
-        (nav as any).userLanguage
-    ];
+	return nav.languages || [
+		nav.language ||
+		(nav as any).browserLanguage ||
+		(nav as any).systemLanguage ||
+		(nav as any).userLanguage
+	];
 }
 
 export function preferredLanguages(): Signal<readonly string[]> {
-    var win = getWindow();
+	var win = getWindow();
 
-    if (!win) {
-        return computed(getEmptyArray);
-    }
+	if (!win) {
+        var headers = inject(RenderConfigService).headers;
+    
+        if (headers === undefined) {
+            return computed(getEmptyArray);
+        }
+        
+        var header = headers["accept-language"];
+        var langs = typeof header === "string" ? header.replace(/\s+/g, "").split(",") : [];
+        
+        for (var i = langs.length; i--;) {
+            var l = langs[i];
+            var p = l.indexOf(";");
+            
+            if (p !== -1) {
+                langs[i] = l.substring(0, p);
+            }
+        }
+        
+        return computed(function(): readonly string[] {
+            return langs;
+        });
+	}
 
-    var nav = win.navigator;
-    
-    var clientLangs = computed(function(): readonly string[] {
-        return getPreferredLanguages(nav);
-    });
-    
-    onDestroy(
-        onEvent(win, "languagechange", function(): void {
-            clientLangs.invalidate();
-        })
-    );
-    
-    return clientLangs;
+	var nav = win.navigator;
+	
+	var clientLangs = computed(function(): readonly string[] {
+		return getPreferredLanguages(nav);
+	});
+	
+	onDestroy(
+		onEvent(win, "languagechange", function(): void {
+			clientLangs.invalidate();
+		})
+	);
+	
+	return clientLangs;
 }
