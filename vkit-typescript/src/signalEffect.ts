b3c770970933b3d96d9d1e60702766642020a6a0
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2025-04-01 20:34:38 +0200

    Inherit parent effect's injector in signalEffect

--- vkit-typescript/src/signalEffect.ts
+++ vkit-typescript/src/signalEffect.ts
@@ -1,5 +1,5 @@
 import { Signal } from "./computed.js";
-import { getEffect, getInjector } from "./contextGuard.js";
+import { getEffect } from "./contextGuard.js";
 import { createEffect, Effect } from "./createEffect.js";
 import { onDestroy } from "./onDestroy.js";
 import { updateEffect } from "./updateEffect.js";
@@ -18,7 +18,7 @@ export function signalEffect<T>(
 	var parentEffect = getEffect(true);
 	
 	if (parentEffect) {
-		var effect = createEffect(parentEffect, getInjector(), function(): void {
+		var effect = createEffect(parentEffect, parentEffect.injector, function(): void {
 			callback(signal.get(), onDestroy);
 		});
 
