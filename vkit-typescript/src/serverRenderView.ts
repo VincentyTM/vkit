eb140da228522180616a82d76c4bbb574d357ad4
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2025-04-11 17:30:46 +0200

    Fix unexpected effect update on server

--- vkit-typescript/src/serverRenderView.ts
+++ vkit-typescript/src/serverRenderView.ts
@@ -1,7 +1,16 @@
+import { getEffect } from "./contextGuard.js";
+import { createEffect } from "./createEffect.js";
 import { ServerElement } from "./createServerElement.js";
-import { ViewTemplate } from "./view.js";
 import { serverRender } from "./serverRender.js";
+import { updateEffect } from "./updateEffect.js";
+import { ViewTemplate } from "./view.js";
 
 export function serverRenderView(container: ServerElement, view: ViewTemplate<unknown, unknown>): void {	
-	serverRender(container, view.getTemplate(view.signal ? view.signal.get() : null));
+	var parentEffect = getEffect();
+
+	var effect = createEffect(parentEffect, parentEffect.injector, function(): void {
+		serverRender(container, view.getTemplate(view.signal ? view.signal.get() : null));
+	});
+
+	updateEffect(effect);
 }
--- vkit-typescript/src/serverRenderViewList.ts
+++ vkit-typescript/src/serverRenderViewList.ts
@@ -1,15 +1,31 @@
+import { getEffect } from "./contextGuard.js";
+import { createEffect, Effect } from "./createEffect.js";
 import { ServerElement } from "./createServerElement.js";
-import { ViewListTemplate } from "./viewList.js";
 import { serverRender } from "./serverRender.js";
+import { updateEffect } from "./updateEffect.js";
+import { ViewListTemplate } from "./viewList.js";
 
 export function serverRenderViewList<T, P extends ParentNode>(
 	element: ServerElement,
-	template: ViewListTemplate<T, P>
+	listTemplate: ViewListTemplate<T, P>
 ): void {
-	var list = template.models.get();
+	var parentEffect = getEffect();
+	var list = listTemplate.models.get();
 	var n = list.length;
 
 	for (var i = 0; i < n; ++i) {
-		serverRender(element, template.getItemTemplate(list[i]));
+		serverRenderViewListBlock(parentEffect, element, listTemplate, list[i]);
 	}
+}
+
+function serverRenderViewListBlock<T, P extends ParentNode>(
+	parentEffect: Effect,
+	element: ServerElement,
+	listTemplate: ViewListTemplate<T, P>,
+	model: T
+): void {
+	var effect = createEffect(parentEffect, parentEffect.injector, function(): void {
+		serverRender(element, listTemplate.getItemTemplate(model));
+	});
+	updateEffect(effect);
 }
