68ba4c9efa237e2949224535dcf5641addd17f82
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2025-04-11 19:13:32 +0200

    Add directive method description

--- vkit-typescript/src/directive.ts
+++ vkit-typescript/src/directive.ts
@@ -20,6 +20,23 @@ function clientRenderDirective<P>(
 	updateEffect(effect);
 }
 
+/**
+ * Creates and returns a directive which can be used to observe or manipulate a DOM element.
+ * Its callback function has a single parameter: the element.
+ * 
+ * Notes:
+ * - The callback runs before the element is inserted into the DOM.
+ * - Directives are evaluated in the browser only.
+ * @example
+ * function ElementLogger() {
+ *     return Div(
+ *         "Hello world",
+ *         directive(div => console.log(div))
+ *     );
+ * }
+ * @param callback The function that is called with the element as a parameter.
+ * @returns The directive.
+ */
 export function directive<P>(callback: (element: P) => void): DirectiveTemplate<P> {
 	return {
 		callback: callback,
