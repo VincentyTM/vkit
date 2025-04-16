670c9eac41ee32fa3d49978c71663f6984ba7559
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2025-04-16 18:11:38 +0200

    Create frameContent method description

--- vkit-typescript/src/frameContent.ts
+++ vkit-typescript/src/frameContent.ts
@@ -5,6 +5,20 @@ import { onEvent } from "./onEvent.js";
 import { render } from "./render.js";
 import { Template } from "./Template.js";
 
+/**
+ * Creates and returns a template which can be used to render content in an iframe.
+ * Note that the window in this context (available with `getWindow()`) is the iframe's own window.
+ * When the iframe's window is unloaded, its contents - including side effects - are destroyed.
+ * @example
+ * Iframe(
+ * 	frameContent(() => "Hello world")
+ * )
+ * @param getTemplate A function that returns a template to be rendered within the iframe.
+ * The function may be wrapped in a signal. In that case, whenever the signal's value changes,
+ * the iframe's contents are re-rendered.
+ * @returns A template that can only be used in an iframe element.
+ * It is not rendered on the server.
+ */
 export function frameContent(getTemplate: (() => Template<HTMLElement>) | Signal<() => Template<HTMLElement>>): Template<HTMLIFrameElement> {
 	var props = {
 		onload: function(this: HTMLIFrameElement): void {
