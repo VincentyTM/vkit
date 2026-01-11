553915975d5e7dc1ae06c04725545b89427bccc6
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2026-01-11 17:19:09 +0100

    Remove svgTags module

--- vkit-typescript/src/index.ts
+++ vkit-typescript/src/index.ts
@@ -107,7 +107,6 @@ export { localStorage, sessionStorage } from "./storage.js";
 export { style } from "./style.js";
 export { styleOutlet } from "./styleOutlet.js";
 export { svgTag, SVGView } from "./svgTag.js";
-export { svgTags } from "./svgTags.js";
 export { Template } from "./Template.js";
 export { tick } from "./tick.js";
 export { timeout } from "./timeout.js";
