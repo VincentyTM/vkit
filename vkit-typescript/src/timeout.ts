6ac918ab09016f8a289b7e26e3ed0c2b63c2f0b9
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2026-01-24 22:01:02 +0100

    Update timeout method description

--- vkit-typescript/src/timeout.ts
+++ vkit-typescript/src/timeout.ts
@@ -3,13 +3,22 @@ import { getGlobalContext } from "./getGlobalContext.js";
 import { onDestroy } from "./onDestroy.js";
 
 /**
- * Sets a timeout in the current global context and clears it when the current reactive context is destroyed.
+ * Schedules a timeout in the current execution context (worker, window, or server app)
+ * that will be canceled if the current reactive context is destroyed. This timeout
+ * will not be set if effects are disabled (for example, during server-side rendering).
+ * 
+ * The provided callback function will be executed once after the specified delay
+ * in milliseconds.
+ * 
  * @example
  * timeout(() => {
  * 	console.log("Timeout is over!");
  * }, 1000);
- * The @param callback function runs once after @param delay milliseconds.
+ * 
+ * @param callback The function to be executed after the specified delay.
+ * @param delay The duration to wait before executing the callback, in milliseconds.
  */
+
 export function timeout(callback: () => void, delay: number): void {
 	effect(function(): void {
 		var global = getGlobalContext();
