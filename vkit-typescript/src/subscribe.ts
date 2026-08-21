359330c215474ce36b4ae3d9b848e62a98488c62
Vincenty <78386807+VincentyTM@users.noreply.github.com>
2026-08-21 18:29:20 +0200

    Remove unused unsubscribe method

--- vkit-typescript/src/subscribe.ts
+++ vkit-typescript/src/subscribe.ts
@@ -11,14 +11,3 @@ export function subscribe(source: ReactiveNode, target: ReactiveNode): void {
 
 	subscribers.push(target);
 }
-
-export function unsubscribe(source: ReactiveNode, target: ReactiveNode): void {
-	var subscribers = source.subscribers;
-
-	for (var i = subscribers.length; i--;) {
-		if (subscribers[i] === target) {
-			subscribers.splice(i, 1);
-			return;
-		}
-	}
-}
